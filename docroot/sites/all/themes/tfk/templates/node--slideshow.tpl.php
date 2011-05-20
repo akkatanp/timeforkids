2c40d20ae77f49aac07cecb2bd2c7fa6606f06eb
hclark <hclark@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-05-20 20:55:54 +0000

    Remove string from template

--- docroot/sites/all/themes/tfk/templates/node--slideshow.tpl.php
+++ docroot/sites/all/themes/tfk/templates/node--slideshow.tpl.php
@@ -101,7 +101,7 @@
     </div>
   <?php endif; ?>
 
-  <div class="content"<?php print $content_attributes; ?>>2864357
+  <div class="content"<?php print $content_attributes; ?>
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
