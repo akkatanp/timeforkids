75fba0ea951a047f4f44da7d8dbbaa460dccb538
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-05-20 19:29:58 +0000

    ATW

--- trunk/docroot/sites/all/themes/tfk/templates/atw-page-list-map-switch.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/atw-page-list-map-switch.tpl.php
@@ -1,2 +1,2 @@
-<?php echo l('List View','around_the_world');?><br/>
-<?php echo l('Map View','around_the_world/map');?><br/>
+<?php echo l('List View','around-the-world');?><br/>
+<?php echo l('Map View','around-the-world/map');?><br/>
