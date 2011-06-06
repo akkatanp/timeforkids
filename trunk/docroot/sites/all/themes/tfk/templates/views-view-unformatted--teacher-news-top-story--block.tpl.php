2fb60246b2e582440d7c2e4968e578eb9c349e0d
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-06 19:21:03 +0000

    html fix

--- trunk/docroot/sites/all/themes/tfk/templates/views-view-unformatted--teacher-news-top-story--block.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/views-view-unformatted--teacher-news-top-story--block.tpl.php
@@ -10,7 +10,6 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  !!!<?php echo $id;?>!!!!
   <div class="<?php print $classes_array[$id]; ?>">
     <?php print $row; ?>
   </div>
