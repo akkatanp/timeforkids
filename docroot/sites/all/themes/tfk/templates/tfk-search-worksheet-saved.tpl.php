e452041def81e42f95fbd472a8d2932fb10f0dab
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-08 23:11:41 +0000

    tfk worksheets

---
+++ docroot/sites/all/themes/tfk/templates/tfk-search-worksheet-saved.tpl.php
@@ -0,0 +1,3 @@
+<div id="mymodaldiv">
+ <?php echo ctools_modal_text_button(t('Save your search(modal)'), 'myfr/ajax/saves/'.$current_str, t('Save your search'),  'ctools-modal-ctools-sample-style'); ?>
+</div>
