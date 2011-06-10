324ca0617ce2608964ac5c228847b782d2c521ed
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-10 17:43:40 +0000

    mini lessons naming fix

--- trunk/docroot/sites/all/themes/tfk/templates/node--atw-sightseeing.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/node--atw-sightseeing.tpl.php
@@ -81,6 +81,7 @@
  * @see template_process()
  */
 ?>
+!!!!!!
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
@@ -119,4 +120,3 @@
   <?php print render($content['comments']); ?>
 
 </div><!-- /.node -->
-!!!!
