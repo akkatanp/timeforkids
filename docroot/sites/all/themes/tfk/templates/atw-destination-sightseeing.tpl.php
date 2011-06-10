8d9ff439c67226f095c7e094936b9e3a7eea85fa
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-10 15:15:46 +0000

    content tabs

--- docroot/sites/all/themes/tfk/templates/atw-destination-sightseeing.tpl.php
+++ docroot/sites/all/themes/tfk/templates/atw-destination-sightseeing.tpl.php
@@ -2,7 +2,11 @@
 <link rel="stylesheet" href="/sites/all/themes/tfk/css/atw-sightseeing.css" />
 <script src="/sites/all/themes/tfk/js/atw-sightseeing.js"></script>
 
-
+<?php if($sightseeing_data['is_admin_editor'] == 1):?>
+<div class="tabs"><h2 class="element-invisible">Primary tabs</h2><ul class="tabs primary clearfix"><li class="active"><a href="/tfk/time/trunk/docroot/destination/spain" class="active"><span class="tab">View</span><span class="element-invisible">(active tab)</span></a></li>
+<li><a href="<?php echo url($sightseeing_data['sightseeing_edit_link']);?>"><span class="tab">Edit</span></a></li>
+</ul></div>
+<?php endif; ?>
 
 
 <div class="atw-graphic-header"></div>
