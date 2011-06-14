759f2160270988988975bc0435e9334a9697cc19
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-14 19:54:56 +0000

    Flags

--- docroot/sites/all/themes/tfk/templates/node--atw-destination.tpl.php
+++ docroot/sites/all/themes/tfk/templates/node--atw-destination.tpl.php
@@ -81,9 +81,9 @@
  * @see template_process()
  */
 
-//print_r($content['field_country_code']);
-$link_to_img = url(drupal_get_path('theme', 'tfk').'/images/flags/flag_'.strtolower($content['field_country_code']['#items'][0]['value']).'.png');
-$content['field_country_code'][0]['#markup'] = '<img src="'.$link_to_img.'"/>';
+//print_r($content['group_destination_images']['field_country_code']);
+$link_to_img = url(drupal_get_path('theme', 'tfk').'/images/flags/flag_'.strtolower($content['group_destination_images']['field_country_code']['#items'][0]['value']).'.png');
+$content['group_destination_images']['field_country_code'][0]['#markup'] = '<img src="'.$link_to_img.'"/>';
 ?>
 <link rel="stylesheet" href="/sites/all/themes/tfk/css/atw_homepage_slideshow.css" />
 <script type="text/javascript" src="/sites/all/themes/tfk/js/atw_homepage_slideshow.js"></script>
