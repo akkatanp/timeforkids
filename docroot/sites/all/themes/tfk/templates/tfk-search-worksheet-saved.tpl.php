1697016ae831b93fa7b6800aa2488eb2ccebb0e9
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-15 20:16:35 +0000

    worksheets

--- docroot/sites/all/themes/tfk/templates/tfk-search-worksheet-saved.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-search-worksheet-saved.tpl.php
@@ -1,3 +1,23 @@
 <div id="mymodaldiv">
- <?php echo ctools_modal_text_button(t('Save your search(modal)'), 'myfr/ajax/saves/'.$current_str, t('Save your search'),  'ctools-modal-ctools-sample-style'); ?>
+  
+</div>
+<form action="" method="post">
+  <div id="savsearches">
+   <?php foreach($saved_searches as $k=>$search):?>
+    <div class="savedsrch">
+      <a href="<?php echo $search['url'];?>"><?php echo $search['name'];?></a>
+      <input type="checkbox" name="<?php echo $search['saved_search_id'];?>"  id="<?php echo $search['saved_search_id'];?>">
+    </div>
+   <?php endforeach;?>
+
+  </div>
+  <div class="submbtn">
+    <input type="submit" value="Submit" />
+  </div>
+</form>
+<br/>
+______________________________
+<br/>
+<div class="savebtn">
+   <?php echo ctools_modal_text_button(t('Save your search(modal)'), 'myfr/ajax/saves/'.$current_str, t('Save your search'),  'ctools-modal-ctools-sample-style'); ?>
 </div>
