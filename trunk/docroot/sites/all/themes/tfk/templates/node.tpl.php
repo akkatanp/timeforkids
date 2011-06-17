295eb94ead9bc2a706a20909f0c3585018b484e9
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-17 23:22:51 +0000

    Theming for 17B (discussion) and 17F (Submit question).

--- trunk/docroot/sites/all/themes/tfk/templates/node.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/node.tpl.php
@@ -84,13 +84,6 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if ($type == 'teacher_community_question'): ?>
-
-    <?php if(isset($top_suggestion_msg)): ?>
-    	<div id='top-suggestion-box'>
-    		<div class='msg'><?php print $top_suggestion_msg; ?></div>
-    		<div class='link'><?php print $top_suggestion_link; ?></div>
-    	</div>
-    <?php endif; ?>
     
     <?php if (!$page && $title): ?>
       <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
