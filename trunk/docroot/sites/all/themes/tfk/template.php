60fec5aa0b92a04f022403f26b2af158d53d3b18
tplunkett <tplunkett@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-21 20:05:56 +0000

    Finalize flashcard filter.

--- trunk/docroot/sites/all/themes/tfk/template.php
+++ trunk/docroot/sites/all/themes/tfk/template.php
@@ -116,7 +116,7 @@ function tfk_preprocess_page(&$variables, $hook) {
 function tfk_preprocess_node(&$variables, $hook) {
   if ($variables['type'] == 'flashcard') {
     foreach (element_children($variables['content']['field_flashcard']) as $delta) {
-      $flashcard = &$flashcards[$delta]['#items'][0];
+      $flashcard = &$variables['content']['field_flashcard'][$delta]['#items'][0];
       $flashcard['data'] = check_markup($flashcard['data'], 'bad_words');
       $flashcard['children'][0] = check_markup($flashcard['children'][0], 'bad_words');
     }
