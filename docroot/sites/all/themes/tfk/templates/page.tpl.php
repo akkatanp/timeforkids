0f33f4e007f7a61b37c3a1ac74e832c8feb16dae
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-25 18:41:32 +0000

    page.tpl.php indentation.

--- docroot/sites/all/themes/tfk/templates/page.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page.tpl.php
@@ -77,18 +77,18 @@ if(drupal_is_front_page()) {
 
 <div id="page-wrapper"><div id="page">
 
-	<div id="header">
-		<div class="section clearfix">
-		<?php if ($logo): ?>
-			<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-		<?php endif; ?>
+  <div id="header">
+    <div class="section clearfix">
+    <?php if ($logo): ?>
+    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+    <?php endif; ?>
                 <?php if (array_key_exists('banner_728x90', $ad)) {echo $ad['banner_728x90'];} ?>
-		</div>
-	</div>
+    </div>
+  </div>
 	
-	<div id="navigation" class="clearfix">
-	
-<div class="section clearfix">
+  <div id="navigation" class="clearfix">
+
+  <div class="section clearfix">
     <?php if ($site_name || $site_slogan): ?>
       <div id="name-and-slogan">
         <?php if ($site_name): ?>
@@ -108,59 +108,59 @@ if(drupal_is_front_page()) {
         <?php endif; ?>
       </div><!-- /#name-and-slogan -->
     <?php endif; ?>
-	
+
     <div id="global-nav-container">
-		<?php print render($page['header']['menu_menu-tfk-global-navigation']); ?>
-                <?php
-                //$form = drupal_get_form('search_form');
-                //$form['basic']['keys']['#title'] = 'lolopk';
-                //print_r($form);
-                  print render(drupal_get_form('search_form'));
-                ?>
-	</div>
-	
-	<div id="teacher-nav-container">
-		<div class="inner">
-			<?php print render($page['header']['views_homepage_username-block']); ?>
-			<?php print render($page['header']['block_3']); ?>
-			<?php print render($page['header']['block_11']); ?>
-			<?php print render($page['header']['menu_menu-tfk-teachers-menu']); ?>
-			<?php print render($page['header']['block_16']); ?>
-                  
-		</div>
-	</div>
+      <?php print render($page['header']['menu_menu-tfk-global-navigation']); ?>
+      <?php
+      //$form = drupal_get_form('search_form');
+      //$form['basic']['keys']['#title'] = 'lolopk';
+      //print_r($form);
+        print render(drupal_get_form('search_form'));
+      ?>
+    </div>
 
-	</div></div><!-- /.section, /#header -->
+    <div id="teacher-nav-container">
+      <div class="inner">
+        <?php print render($page['header']['views_homepage_username-block']); ?>
+        <?php print render($page['header']['block_3']); ?>
+        <?php print render($page['header']['block_11']); ?>
+        <?php print render($page['header']['menu_menu-tfk-teachers-menu']); ?>
+        <?php print render($page['header']['block_16']); ?>
+      </div>
+    </div>
 
-  <div id="main-wrapper"><div id="main" class="clearfix<?php if ($main_menu || $page['navigation']) { print ' with-navigation'; } ?>">
-	<?php print render($page['sidebar_second']); ?>
-  
-  <?php if(!$is_front):?>
-    <h1 class="title" id="section-title">
-    	<?php if(isset($section_title)):?>
-    		<?php print $section_title; ?>
-    	<?php endif; ?>
-  	</h1>
-  <?php endif;?>
+  </div></div><!-- /.section, /#header -->
 
-	<?php if(array_key_exists('sidebar_top', $rendered)): ?>
-		<?php print $rendered['sidebar_top']; ?>
-	<?php endif; ?>
-	
-	<?php print render($page['sidebar_first']); ?>
-  <?php if(array_key_exists('editor_menu', $page)): ?>
-  	<?php print render($page['editor_menu']); ?>
-  <?php endif; ?>
-  <?php if(array_key_exists('before_content', $page)): ?>
-  	<?php print render($page['before_content']); ?>
-  <?php endif; ?>
-  
-	<div id="content" class="column"><div class="section">
-		
-      <?php if(isset($page_suggestion_box)): ?>
-      	<?php print $page_suggestion_box; ?>
+  <div id="main-wrapper">
+    <div id="main" class="clearfix<?php if ($main_menu || $page['navigation']) { print ' with-navigation'; } ?>">
+      <?php print render($page['sidebar_second']); ?>
+
+      <?php if(!$is_front):?>
+        <h1 class="title" id="section-title">
+          <?php if(isset($section_title)):?>
+            <?php print $section_title; ?>
+          <?php endif; ?>
+        </h1>
       <?php endif;?>
-    
+
+      <?php if(array_key_exists('sidebar_top', $rendered)): ?>
+        <?php print $rendered['sidebar_top']; ?>
+      <?php endif; ?>
+
+      <?php print render($page['sidebar_first']); ?>
+      <?php if(array_key_exists('editor_menu', $page)): ?>
+        <?php print render($page['editor_menu']); ?>
+      <?php endif; ?>
+      <?php if(array_key_exists('before_content', $page)): ?>
+        <?php print render($page['before_content']); ?>
+      <?php endif; ?>
+      
+      <div id="content" class="column"><div class="section">
+  
+      <?php if(isset($page_suggestion_box)): ?>
+        <?php print $page_suggestion_box; ?>
+      <?php endif;?>
+  
       <?php print render($page['highlighted']); ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
@@ -177,23 +177,23 @@ if(drupal_is_front_page()) {
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      
+        
       <?php if(isset($top_suggestion_msg)): ?>
-      	<div id='top-suggestion-box' class="suggestion-box">
-      		<div class='msg'><?php print $top_suggestion_msg; ?></div>
-      		<div class='link'><?php print $top_suggestion_link; ?></div>
-      	</div>
+        <div id='top-suggestion-box' class="suggestion-box">
+          <div class='msg'><?php print $top_suggestion_msg; ?></div>
+          <div class='link'><?php print $top_suggestion_link; ?></div>
+        </div>
       <?php endif; ?>
       
       <?php print render($page['content']); ?>
-
+  
       <?php print $feed_icons; ?>
-    </div></div><!-- /.section, /#content -->
+      </div></div><!-- /.section, /#content -->
 
-  </div>
+    </div><!-- /#main -->
 
   <?php print render($page['footer']); ?>
-  </div><!-- /#main, /#main-wrapper -->
+  </div><!-- /#main-wrapper -->
 
 </div></div><!-- /#page, /#page-wrapper -->
 
