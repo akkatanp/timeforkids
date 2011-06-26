fef4a17066128340abc4753cc1c9627181f3f319
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-06-26 18:47:41 +0000

    Format conditional in PHPTemplate format. Check for array existance before checking for it's keys. Use print over echo for consistency.

--- trunk/docroot/sites/all/themes/tfk/templates/page.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/page.tpl.php
@@ -80,9 +80,11 @@ if(drupal_is_front_page()) {
   <div id="header">
     <div class="section clearfix">
     <?php if ($logo): ?>
-    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+    	<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
     <?php endif; ?>
-                <?php if (array_key_exists('banner_728x90', $ad)) {echo $ad['banner_728x90'];} ?>
+    <?php if(isset($ad) && array_key_exists('banner_728x90', $ad)): ?>
+    	<?php print $ad['banner_728x90']; ?>
+    <?php endif; ?>
     </div>
   </div>
 	
