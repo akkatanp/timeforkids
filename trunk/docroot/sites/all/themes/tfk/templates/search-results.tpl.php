3979e7ab5c91751f2dec31121244cbde73a774ba
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-05 18:49:56 +0000

    Refactor site-wide search template.

--- trunk/docroot/sites/all/themes/tfk/templates/search-results.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/search-results.tpl.php
@@ -30,15 +30,14 @@
     	c<?php print $pager_top; ?>
     <?php endif; ?>!
     
-    <ol id="tfk-search-results-photos-video" class="search-results<?php if($module): ?><?php print $module; ?>-results<?php endif; ?>">
+    <ol id="tfk-search-results-photos-video" class="search-results<?php if($module): ?><?php print $module; ?>-results<?php endif; ?>" class="search-results">
       <?php print $search_results; ?>
     </ol>
     <?php print $pager; ?>
     
   <?php else: ?>
     
-    <h2><?php print t('Search results');?></h2>
-    <ol id="<?php print ($ordered_list_id) ? $ordered_list_id: 'search-results'; ?>">
+    <ol id="<?php print ($ordered_list_id) ? $ordered_list_id: 'search-results'; ?>" class="search-results">
       <?php print $search_results; ?>
     </ol>
     
