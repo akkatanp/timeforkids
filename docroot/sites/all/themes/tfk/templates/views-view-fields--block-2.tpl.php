c09e7abc110108c0a4c3afb040bb94f6f284be2f
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-06 18:44:31 +0000

    homepage fixes

--- docroot/sites/all/themes/tfk/templates/views-view-fields--block-2.tpl.php
+++ docroot/sites/all/themes/tfk/templates/views-view-fields--block-2.tpl.php
@@ -39,11 +39,11 @@
 
 
 
-<?php if(strlen($fields['field_minisite_resources']->content) != 0):?>
+<?php if(strlen($fields['field_related_articles']->content) != 0):?>
     <div class="related-content-wrap">
-        <div class="addit-content"><?php print $fields['field_minisite_resources_label']->content;?></div>
-        <?php if($fields['field_minisite_resources']): ?>
-           <div class="addit-mini-lessons">WORKSHEETS : <?php print $fields['field_minisite_resources']->content;?></div>
+        <div class="addit-content"><?php print $fields['field_related_articles_1']->content;?></div>
+        <?php if($fields['field_related_articles']): ?>
+           <div class="addit-mini-lessons"><?php print $fields['field_related_articles']->content;?></div>
         <?php endif; ?>
 
     </div>
