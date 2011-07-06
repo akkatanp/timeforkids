9566059ada049f8aa4bdf87f63914c9ed3c0f6fa
tplunkett <tplunkett@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-06 18:20:54 +0000

    Ticket #125: Fix print/save buttons, and the rest of aplus_papers.

--- docroot/sites/all/modules/custom/homework_helper/aplus_papers/js/aplus_papers.js
+++ docroot/sites/all/modules/custom/homework_helper/aplus_papers/js/aplus_papers.js
@@ -10,13 +10,13 @@
 
       $('#aplus-print-save-buttons input, .a-plus-overview a').click(function() {
         step = false;
-      });  
+      });
 
       window.onbeforeunload = function() {
         if (step == true) {
           return 'If you leave the page you will lose everything you have written and worked on so far. Are you sure you want to leave?';
         }
       }
-    } 
+    }
   }
 }(jQuery));
