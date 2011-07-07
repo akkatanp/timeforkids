5276e211c589aa559f1e566c06817a7ce1b552a3
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-07 22:07:48 +0000

    Block CSS CLasses

--- trunk/docroot/sites/all/themes/tfk/templates/block.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/block.tpl.php
@@ -47,7 +47,7 @@
  * @see zen_process_block()
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> <?php print $block_classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
