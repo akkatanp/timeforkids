2cda8235f9d0c692463978042a84c308d7c77f57
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-12 17:35:42 +0000

    Push trunk to test as tag tags/2011-07-12

---
+++ tags/2011-07-12/docroot/sites/all/themes/tfk/js/tfk_global.js
@@ -0,0 +1,54 @@
+/* Global JS file */
+var $ = jQuery;
+
+$(document).ready(function() {
+	//lightbox
+	$('#login-link').loginBox();
+	
+	//hide notification if it is there
+	$('#hide-notification').click(function(){
+		$.ajax({
+			url: $(this).attr('rel'),
+			success: function(data) {
+				if (data == 'success') {
+					$('.notification').slideUp('fast');
+				}
+			}
+		});
+	});
+});
+
+(function($) {
+	$.fn.loginBox = function() {		
+		$(this).click(function(e) {
+			e.preventDefault();
+			createLightBox();
+		});
+		
+		var createLightBox = function() {
+			$('body').css('overflow', 'hidden');
+			
+			var mask = $('<div></div>').attr('id', 'mask').css({
+				'height': $(window).height() + 'px',
+				'top': $(window).scrollTop() + 'px'
+			}).appendTo($(document.body));
+			
+			var lightBox = $('<div></div>').attr('id', 'lightbox').css({
+				'top': (($(window).height() / 2) - 175) + $(window).scrollTop() + 'px',
+				'left': (($(window).width() / 2) - 350) + 'px'
+			}).appendTo($(document.body));
+			
+			var loginForm = $('#login-container').html();
+			var loginContainer = $('<div></div>').attr('id', 'login-container').html(loginForm).appendTo(lightBox);
+			var loginHeader = $('#lightbox #login-header');
+			var closeButton = $('<a></a>').addClass('close-button').click(closeLightBox).appendTo(loginHeader);
+		};
+		
+		var closeLightBox = function(e) {
+			e.preventDefault();
+			$('#lightbox').remove();
+			$('#mask').remove();
+			$('body').css('overflow', 'auto');
+		}
+	}
+})($);
---
+++ tags/2011-07-12/docroot/sites/all/themes/tfk/templates/atw-timeline-node-slideshow.tpl.php
@@ -0,0 +1,55 @@
+<?php //echo $cust_node_title;?>
+
+	<div id="event-year"></div>
+	<div id="line-above"></div>
+	<div id="event-timeline-bar-container">
+		<div id="prev"></div>
+		<div id="event-timeline-bar"></div>
+		<div id="next"></div>
+	</div>
+	<div id="line-below"></div>
+	<div id="event-container">
+		<?php $i = 0; ?>
+		<?php foreach($images_data as $image):?>
+		
+		<div class="event" id="event-<?php print $i ?>"<?//php if ($i != 0) echo ' style="display: none;"'; ?>>
+		
+			<div class="event-image"><img src="<?php print $image['image_path'] ?>" /></div>
+
+			<?php if(isset($image['image_credit'])):?><div class="event-credit"><?php print $image['image_credit'];?></div><?php endif;?>
+			<div class="event-text">
+				<h3><?php print $image['event_title'] ?></h3>
+				<?php print $image['event_copy'] ?>
+				
+				<?php if($is_admin_editor == 1):?>
+				<?php
+                                //echo $return_path;
+                                echo '<a href="'.url('field-collection/field-timeline-event/'.$image['event_nid'].'/edit',array('query'=>array('destination' => $return_path))).'">[edit]</a>';
+                                echo ' - <a href="'.url('field-collection/field-timeline-event/'.$image['event_nid'].'/delete',array('query'=>array('destination' => $return_path))).'">[delete]</a>';
+                                ?>
+                                <!-- MIKE : <?php print $image['image_shape'] // can be "undefined" , "horizontal","vertical","square" ?> -->
+				<?php endif;?>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		
+		<?php $i++; ?>
+		<?php endforeach; ?>
+		<div class="clearfix"></div>
+	</div>
+
+<!--
+THIS IS RELATED ARTICLES LIST:
+<?php foreach($related_content as $related_node):?>
+	<?php print l($related_node['title'],'node/'.$related_node['nid'],array('attributes' => array('class' =>'anyclassyouwant'))); ?>
+	<br/>
+<?php endforeach; ?>
+-->
+
+<?php if($is_admin_editor == 1):?>
+    <?php print l('ADD NEW TIMELINE EVENT','field-collection/field-timeline-event/add/node/'.$timeline_nid);?>
+<?php endif;?>
+
+
+
+    
