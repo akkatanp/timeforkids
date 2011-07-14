ebd6d6c1070175b4bffe607a94c6e20911f97fdd
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-14 16:53:37 +0000

    Branch clean head trunk before applying 7.4 updates.

---
+++ branches/7.4/trunk/docroot/sites/all/themes/tfk/js/atw-daylife.js
@@ -0,0 +1,93 @@
+var $ = jQuery;
+
+$(document).ready(function() {
+	$('#daylife-container').daylife();
+});
+
+(function($) {
+	$.fn.daylife = function() {
+		var daylifebar = $('#daylife-bar');
+		var inner = daylifebar.find('#inner');
+		var activities = inner.children();
+		var bottomline = $('#line-below');
+		var thisTime = $('#current-time');
+		var prev = $('#prev');
+		var next = $('#next');
+		var maxActivities = 10;
+		var activityWidth = 46;
+		var leftActivity = 0;
+		
+		var showHideActivity = function() {
+			activities.each(function(i, activity) {
+				$(activity).removeClass('active');
+			});
+			$(this).addClass('active');
+			$('#activity-time').text($(this).attr('title'));
+			$('#activity-text').text($(this).attr('rel'));
+			thisTime.text($(this).attr('title'));
+			
+			linePos = $(this).position().left + (activityWidth / 2) + 14;
+			bottomline.animate({'left': linePos + 'px'}, 500);
+			
+			timePos = linePos - (thisTime.width() / 2) - 7;
+			if (timePos < 0) timePos = 0;
+			thisTime.animate({'left': timePos + 'px'}, 500);
+		};
+		
+		var prevEvent = function() {
+			var activity = inner.find('.active');
+			var activityNum = parseInt(activity.attr('id').split('-')[1]);
+			if (activityNum == 0) {
+				var prevNum = activities.length;
+			} else {
+				var prevNum = activityNum - 1;
+			}
+			
+			if (prevNum < leftActivity) {
+				inner.animate({'left': ((leftActivity - maxActivities - 1) * activityWidth) + 'px'}, 500, function() {
+					$('#clock-' + prevNum).trigger('click');
+				});
+				leftActivity = leftActivity - maxActivities - 1;
+			} else {
+				$('#clock-' + prevNum).trigger('click');
+			}
+		}
+		
+		var nextEvent = function() {
+			var activity = inner.find('.active');
+			var activityNum = parseInt(activity.attr('id').split('-')[1]);
+			if (activityNum == activities.length) {
+				var nextNum = 0;
+			} else {
+				var nextNum = activityNum + 1;
+			}
+			
+			if (nextNum > maxActivities - 1) {
+				inner.animate({'left': (-1 * maxActivities * activityWidth) + 'px'}, 500, function() {
+					$('#clock-' + nextNum).trigger('click');
+				});
+				leftActivity = maxActivities + 1;
+			} else {
+				$('#clock-' + nextNum).trigger('click');
+			}
+		}
+		
+		if (activities.length) {
+			activities.each(function(i, activity) {
+				if (i == 0) {
+					$(activity).addClass('active');
+					$('#activity-time').text($(activity).attr('title'));
+					$('#activity-text').text($(activity).attr('rel'));
+					thisTime.text($(activity).attr('title'));
+					
+					linePos = $(activity).position().left + ($(activity).width() / 2);
+					bottomline.css('left', linePos + 'px');
+				}
+				$(activity).click(showHideActivity);
+			});
+			
+			prev.click(prevEvent);
+			next.click(nextEvent);
+		}
+	};
+})($);
---
+++ branches/7.4/trunk/docroot/sites/all/themes/tfk/js/atw-timeline.js
@@ -0,0 +1,117 @@
+var $ = jQuery;
+
+$(document).ready(function() {
+	$('#event-container').timeline();
+});
+
+(function($) {
+	$.fn.timeline = function() {
+		var events = this.children();
+		events = events.not('.clearfix');
+		var numEvents = events.length;
+		var eventContainer = $('#event-container');
+		var year = $('#event-year');
+		var timebar = $('#event-timeline-bar');
+		var inner = timebar.find('#inner');
+		var topline = $('#line-above');
+		var bottomline = $('#line-below');
+		var prev = $('#prev');
+		var next = $('#next');
+		var maxNodes = 19;
+		var nodeWidth = 24;
+		var leftNode = 0;
+		
+		var showHideEvent = function() {
+			nodes.each(function(i, node) {
+				$(node).removeClass('active');
+			});
+			
+			events.each(function(i, event) {
+				$(event).fadeOut(500);
+			});
+			
+			var nodeNum = parseInt(this.id.split('-')[2]);
+			var thisEvent = events.eq(nodeNum)
+			thisEvent.fadeIn(500);
+			year.text(thisEvent.find('.event-text h3').text());
+			$(this).addClass('active');
+			
+			linePos = $(this).position().left + ($(this).width() / 2) + 16;
+			
+			if (nodeNum > maxNodes -1) {
+				linePos = linePos - (maxNodes * nodeWidth);
+			}
+			
+			topline.animate({'left': linePos + 'px'}, 500);
+			bottomline.animate({'left': linePos + 'px'}, 500);
+			
+			yearPos = linePos - (year.width() / 2) - 7;
+			if (yearPos < 0) yearPos = 0;
+			year.animate({'left': yearPos + 'px'}, 500);
+		}
+		
+		var prevEvent = function() {
+			var numNodes = nodes.length - 1;
+			var activeNode = inner.find('.active');
+			var nodeNum = parseInt(activeNode.attr('id').split('-')[2]);
+			if (nodeNum == 0) {
+				var prevNum = numNodes;
+			} else {
+				var prevNum = nodeNum - 1;
+			}
+			
+			if (prevNum < leftNode) {
+				inner.animate({'left': ((leftNode - maxNodes - 1) * nodeWidth) + 'px'}, 500, function() {
+					$('#event-node-' + prevNum).trigger('click');
+				});
+				leftNode = leftNode - maxNodes - 1;
+			} else {
+				$('#event-node-' + prevNum).trigger('click');
+			}
+		}
+		
+		var nextEvent = function() {
+			var numNodes = nodes.length - 1;
+			var activeNode = inner.find('.active');
+			var nodeNum = parseInt(activeNode.attr('id').split('-')[2]);
+			if (nodeNum == numNodes) {
+				var nextNum = 0;
+			} else {
+				var nextNum = nodeNum + 1;
+			}
+			
+			if (nextNum > maxNodes - 1) {
+				inner.animate({'left': (-1 * maxNodes * nodeWidth) + 'px'}, 500, function() {
+					$('#event-node-' + nextNum).trigger('click');
+				});
+				leftNode = maxNodes + 1;
+			} else {
+				$('#event-node-' + nextNum).trigger('click');
+			}
+		}
+		
+		if (numEvents) {
+			for (i=0;i<numEvents;i++) {
+				var node = $('<div></div>').attr('id', 'event-node-' + i).addClass('event-node').appendTo(inner);
+				if (i == 0) {
+					node.addClass('active');
+					year.text($('#event-0').find('.event-text h3').text());
+					linePos = node.position().left + (node.width() / 2) + 16;
+					topline.css('left', linePos + 'px');
+					bottomline.css('left', linePos + 'px');
+				} else {
+					$('#event-' + i).hide();
+				}
+			}
+			
+			var nodes = inner.children();
+			inner.width(nodes.length * nodeWidth);
+			nodes.each(function(i, node) {
+				$(node).click(showHideEvent);
+			});
+			
+			prev.click(prevEvent);
+			next.click(nextEvent);
+		}
+	};
+})($);
