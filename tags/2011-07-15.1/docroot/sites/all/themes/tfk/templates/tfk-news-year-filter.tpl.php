57afe8249d19787d7efb4736cfc3ff185978e77f
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-15 19:42:30 +0000

    Push trunk to test as tag tags/2011-07-15.1

---
+++ tags/2011-07-15.1/docroot/sites/all/themes/tfk/templates/tfk-news-year-filter.tpl.php
@@ -0,0 +1,18 @@
+
+<div id="news-sidebar-year-filter">
+  
+<form action="">
+	<p>Select a date range</p>
+	<label for="yeardropdown">Year</label>
+	<select id="yeardropdown" name="listofyears">
+          <option id="news-all" value="all">All</option>
+    <?php foreach($list_of_years as $year): ?>
+		<option id="<?php echo $year;?>"><?php echo $year;?></option>
+    <?php endforeach; ?>
+	</select>
+	<div class="button-holder">
+		<input type="button" id="yearsubmit" onClick="location.href='<?php echo url('news-archive')?>/'+ document.getElementById('yeardropdown').value;" value="Submit Date Range">	
+	</div>
+	<div class="clearfix"></div>
+</form>
+</div>
