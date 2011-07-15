1127e931beeee2d32cf33864fdcab423dc65c289
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-15 15:46:07 +0000

    tfks57

--- trunk/docroot/sites/all/themes/tfk/templates/tfk-news-taxonomy-links.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/tfk-news-taxonomy-links.tpl.php
@@ -1,6 +1,7 @@
 <?php echo l('NEWS ARCHIVE','news-archive');?>
 
 <ul class="menu">
+  <li><?php echo l($term['term_name'],'news-archive/all',array('attributes' => array('class'=>array($term['class']))));?></li>
 <?php foreach($terms_with_articles as $term): ?>
   <li><?php echo l($term['term_name'],'news-archive/'.strtolower($term['term_name']),array('attributes' => array('class'=>array($term['class']))));?></li>
 <?php endforeach; ?>
