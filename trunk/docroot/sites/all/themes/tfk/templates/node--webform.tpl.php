1695562c795a87f070567f716a2810cf2d70ac8a
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-19 15:34:56 +0000

    tfks135

--- trunk/docroot/sites/all/themes/tfk/templates/node--webform.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/node--webform.tpl.php
@@ -88,7 +88,7 @@
 
   <?php print render($title_prefix); ?>
 
-  !<?php print $title; ?>X
+  <h2><?php print $title; ?></h2>
 
   <?php if (!$page && $title): ?>
     <!--<h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>-->
