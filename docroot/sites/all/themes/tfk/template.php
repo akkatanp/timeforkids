017e5d7f9c69e1cb030300dbcfce653a5cb73f83
aackelsberg <aackelsberg@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-25 18:11:58 +0000

    Allow answer of '0' on flashcards

--- docroot/sites/all/themes/tfk/template.php
+++ docroot/sites/all/themes/tfk/template.php
@@ -138,7 +138,8 @@ function tfk_preprocess_node(&$variables, $hook) {
     foreach (element_children($variables['content']['field_flashcard']) as $delta) {
       $flashcard = &$variables['content']['field_flashcard'][$delta]['#items'][0];
       $flashcard['data'] = check_markup($flashcard['data'], 'bad_words');
-      $flashcard['children'][0] = check_markup($flashcard['children'][0], 'bad_words');
+      // Flashcards with an answer of '0' do not disaply correctly if run through the bad_words filter.
+      $flashcard['children'][0] = is_numeric($flashcard['children'][0]) ? $flashcard['children'][0] : check_markup($flashcard['children'][0], 'bad_words');
     }
   }
 }
