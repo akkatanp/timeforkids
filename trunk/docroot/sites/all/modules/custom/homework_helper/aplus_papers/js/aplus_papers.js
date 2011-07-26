0ce20d30e6fd03f18d7afc90344631952ad8b800
sheise <sheise@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-26 17:39:55 +0000

    aplus warning on title and date field

--- trunk/docroot/sites/all/modules/custom/homework_helper/aplus_papers/js/aplus_papers.js
+++ trunk/docroot/sites/all/modules/custom/homework_helper/aplus_papers/js/aplus_papers.js
@@ -13,6 +13,13 @@
       });
 
       window.onbeforeunload = function() {
+        if ($("input").hasClass('aplus-name') && $('.aplus-name').val() || $("input").hasClass('aplus-date') && $('.aplus-date').val()) {
+          return 'If you leave this page, you will lose everything you have written so far. Are you sure you want to leave?';
+        }  
+        else {
+          step = false;
+        }  
+        
         if (step == true) {
           return 'If you leave this page, you will lose everything you have written so far. Are you sure you want to leave?';
         }
