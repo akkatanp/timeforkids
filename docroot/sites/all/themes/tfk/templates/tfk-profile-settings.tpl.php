0452942f7c6715695e95e93b73531ad4edd694cc
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-27 19:41:07 +0000

    tfks202

---
+++ docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
@@ -0,0 +1,10 @@
+<div class="usersettings">
+   <div class="settingsheader">
+     SETTINGS
+   </div>
+   <div class="settingslink">
+     <a href="<?php echo url('');?>" class="active">Profile</a>
+     <a href="" class="active">Change Password</a>
+     <a href="" class="active">Subscription</a>
+   </div>
+</div>
