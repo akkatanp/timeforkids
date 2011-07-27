335521093a8808c3bfabb83759e7bec126e927f1
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-27 19:28:41 +0000

    Push trunk to test as tag tags/2011-07-27

---
+++ tags/2011-07-27/docroot/sites/all/modules/custom/tfk_favorites/js/tfk_favorites.js
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * Used by block template for theme('tfk_favorites_links', $variables).
+ *
+ * @see tfk_favorites_theme()
+ * @see tfk_favorites_block_view()
+ * @see _tfk_favorites_links_block_content()
+ * @see tfk-favorites-links.tpl.php
+ */
+ 
+/**
+ * Favorites Behavior.
+ */
+(function ($) {
+  Drupal.behaviors.tfk_favorites = {
+    attach: function (context, settings) {
+
+      // Delete favorites listener.
+      $('#delfavs', context).click(function(){
+        var confirmation = confirm("Are you sure you want to delete your favorites?");
+        if(confirmation) {
+          $.ajax({
+            url: settings.tfk_favorites.ajax_callback,
+            success: function(data) {
+              if(data == settings.tfk_favorites.success){
+                $('.throbber').hide();
+                location.href = settings.tfk_favorites.ref;
+              }
+            }
+          });
+          $('.throbber').show();
+        }
+      });
+      
+      // Delete this search.
+      $('#deleteThisSearch', context).click(function(event){
+        var confirmation = confirm("Are you sure you want to delete this search?");
+        if(!confirmation) {
+          event.preventDefault();
+        } else {
+          // Let it run it's course.
+        }
+      });  
+
+    }
+  };
+}(jQuery));
---
+++ tags/2011-07-27/docroot/sites/all/themes/tfk/templates/atw-page-list-map-switch.tpl.php
@@ -0,0 +1,5 @@
+<div class="atw-switcher-text">Destinations By World Region</div>
+<div class="atw-switcher-links">
+<?php echo l('Map View','around-the-world/map',array('attributes' => array('class' => array('map-view'))));?><br/>
+<?php echo l('List View','around-the-world',array('attributes' => array('class' => array('list-view'))));?><br/>
+</div>
