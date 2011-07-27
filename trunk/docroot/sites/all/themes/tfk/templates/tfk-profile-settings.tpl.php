42b5b484b7b01af3fb7c9caa0cd1efa45b93300b
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-27 19:49:48 +0000

    tfks201

--- trunk/docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
@@ -4,7 +4,11 @@
    </div>
    <div class="settingslink">
      <a href="<?php echo url('');?>" class="active">Profile</a>
-     <a href="" class="active">Change Password</a>
+   </div>
+   <div class="settingslink">
+    <a href="" class="active">Change Password</a>
+   </div>
+   <div class="settingslink">
      <a href="" class="active">Subscription</a>
    </div>
 </div>
