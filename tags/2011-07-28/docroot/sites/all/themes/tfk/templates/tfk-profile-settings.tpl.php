b1feeaf89fe1ed7e410e5cf6fb798b3261f4a4b7
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-28 15:01:34 +0000

    Push trunk to test as tag tags/2011-07-28

---
+++ tags/2011-07-28/docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
@@ -0,0 +1,14 @@
+<div class="usersettings">
+   <div class="settingsheader">
+     SETTINGS
+   </div>
+   <div class="settingslink">
+     <a href="<?php echo url('');?>" class="active">Profile</a>
+   </div>
+   <div class="settingslink">
+    <a href="" class="active">Change Password</a>
+   </div>
+   <div class="settingslink">
+     <a href="" class="active">Subscription</a>
+   </div>
+</div>
