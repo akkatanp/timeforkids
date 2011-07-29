48e5acc4f0904da19132a81d53a9d5b945b3fc5e
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-29 19:08:11 +0000

    tfks247

--- docroot/sites/all/themes/tfk/templates/tfk-not-found-page.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-not-found-page.tpl.php
@@ -1,1 +1,4 @@
-OLOLOLOLO
+<h2 class="not-found-header">Oops!</h2>
+<p class="not-found-not-available">Looks like the page you are looking for has been moved, or is no longer available.</p>
+<p><span class="not-found-click"><a href="/">Click here</a></span> to go back to the <strong>Homepage</strong></p>
+<p class="not-found-or">or</p><p>run a search here.</p>
