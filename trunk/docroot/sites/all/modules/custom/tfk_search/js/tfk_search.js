4b57c1b8a72a625899ab9be1a3b69d7bff07d943
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-07-29 17:54:04 +0000

    tfks160: Move inline JavaScript into a Drupal behavior.

---
+++ trunk/docroot/sites/all/modules/custom/tfk_search/js/tfk_search.js
@@ -0,0 +1,30 @@
+/**
+ * @file TFK Search behaviors.
+ */
+(function ($) {
+
+  Drupal.behaviors.tfk_search = {
+    attach: function (context, settings) {
+
+      var sFields = new Array();
+      $(".search-form input.form-text", context).each(function(index, Element) {
+        
+        // Convert Element into a JQuery object and store it in an array to
+        // reduce expensive calls to $.
+        sFields[index] = $(Element, context);
+        
+        sFields[index].val('Search').blur(function() {
+          if(sFields[index].val() == '') {
+            sFields[index].val('Search');
+          }
+        }).focus(function() {   
+          if(sFields[index].val() == 'Search') {
+            sFields[index].val('');
+          }
+        });
+        
+      });      
+      
+    }
+  };
+}(jQuery));
