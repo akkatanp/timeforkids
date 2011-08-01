c9358926516bf61542bbeeac457409b4d9e447db
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-01 22:56:55 +0000

    tfks289

--- docroot/sites/all/themes/tfk/template.php
+++ docroot/sites/all/themes/tfk/template.php
@@ -180,17 +180,54 @@ function tfk_pager($variables) {
       );
     }
 
-
-    echo $li_last;
-
-    return '!!!!<h2 class="element-invisible">' . t('Pages') . '</h2>' . theme('item_list', array(
+    return '<h2 class="element-invisible">' . t('Pages') . '</h2>' . theme('item_list', array(
       'items' => $items,
       'attributes' => array('class' => array('pager')),
     ));
   }
 }
 
+function tfk_pager_link($variables) {
+  $text = $variables['text'];
+  $page_new = $variables['page_new'];
+  $element = $variables['element'];
+  $parameters = $variables['parameters'];
+  $attributes = $variables['attributes'];
 
+  $page = isset($_GET['page']) ? $_GET['page'] : '';
+  if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
+    $parameters['page'] = $new_page;
+  }
+
+  $query = array();
+  if (count($parameters)) {
+    $query = drupal_get_query_parameters($parameters, array());
+  }
+  if ($query_pager = pager_get_query_parameters()) {
+    $query = array_merge($query, $query_pager);
+  }
+
+  // Set each pager link title
+  if (!isset($attributes['title'])) {
+    static $titles = NULL;
+    if (!isset($titles)) {
+      $titles = array(
+        t('« First') => t('Go to first page'),
+        t('‹ Previous') => t('Go to previous page'),
+        t('Next ›') => t('Go to next page'),
+        t('Last »') => t('Go to last page'),
+      );
+    }
+    if (isset($titles[$text])) {
+      $attributes['title'] = $titles[$text];
+    }
+    elseif (is_numeric($text)) {
+      $attributes['title'] = t('Go to page @number', array('@number' => $text));
+    }
+  }
+
+  return l($text, $_GET['q'], array('attributes' => $attributes, 'query' => $query));
+}
 /**
  * Override or insert variables into the page templates.
  *
