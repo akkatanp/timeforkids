9a6f1a0124f611fdd8d0f384b929a99744e59255
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-04 15:24:02 +0000

    tfks315

--- trunk/docroot/sites/all/themes/tfk/templates/atw-destination-navigation.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/atw-destination-navigation.tpl.php
@@ -6,6 +6,6 @@
 		<?php if($navigation_data['timeline_exists'] == 1):?><li><?php echo l('History Timeline',$navigation_data['alias'].'/history-timeline',array('attributes'=> array('class'=>$navigation_data['timeline_class'])));?></li><?php endif;?>
 		<?php if($navigation_data['lingo_exists'] == 1):?><li><?php echo l('Native Lingo',$navigation_data['alias'].'/native-lingo',array('attributes'=> array('class'=>$navigation_data['lingo_class'])));?></li><?php endif;?>
 		<?php if($navigation_data['challenge_exists'] == 1):?><li><?php echo l('Challenge',$navigation_data['alias'].'/challenge',array('attributes'=> array('class'=>$navigation_data['challenge_class'])));?></li><?php endif;?>
-		<?php if($navigation_data['daylife_exists'] == 1):?><li><?php echo l('Day in Life',$navigation_data['alias'].'/day-in-life',array('attributes'=> array('class'=>$navigation_data['daylife_class'])));?></li><?php endif;?>
+		<?php if($navigation_data['daylife_exists'] == 1):?><li><?php echo l('Day in the Life',$navigation_data['alias'].'/day-in-life',array('attributes'=> array('class'=>$navigation_data['daylife_class'])));?></li><?php endif;?>
 	</ul>
 </div>
