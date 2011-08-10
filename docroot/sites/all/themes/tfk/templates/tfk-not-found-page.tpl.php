4091f7e04907bf26cf03fd5db7b432af89f1bc33
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-10 18:51:45 +0000

    tfks359

--- docroot/sites/all/themes/tfk/templates/tfk-not-found-page.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-not-found-page.tpl.php
@@ -1,3 +1,11 @@
+<script>
+  $(document).ready(function(){
+
+    $('#edit-search-block-form--2').val('Search');
+    alert(2);
+
+  });
+</script>
 <h2 class="not-found-header">Oops!</h2>
 <p class="not-found-not-available">Looks like the page you are looking for has been moved, or is no longer available.</p>
 <p><span class="not-found-click"><a href="/">Click here</a></span><span class="not-found-go-back"> to go back to the <strong>Homepage</strong></span></p>
