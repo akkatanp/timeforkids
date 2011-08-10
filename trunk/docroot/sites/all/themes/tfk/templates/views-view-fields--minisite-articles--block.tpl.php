0b95b56e8bd2d5603e0641821d92ec482363097c
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-10 15:05:45 +0000

    tfks350

--- trunk/docroot/sites/all/themes/tfk/templates/views-view-fields--minisite-articles--block.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/views-view-fields--minisite-articles--block.tpl.php
@@ -30,7 +30,7 @@
     <?php endif; ?>
 
     <?php print $field->wrapper_prefix; ?>
-      !<?php print $field->wrapper_prefix;?>!
+      !<?php print $view;?>!
       <?php print $field->label_html; ?>
       <?php print $field->content; ?>
     <?php print $field->wrapper_suffix; ?>
