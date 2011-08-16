66be30aba1bb7526282d74e6fb01f8499732e769
hclark <hclark@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-16 14:03:31 +0000

    Remove hard coded form

--- docroot/sites/all/themes/tfk/templates/page--user.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page--user.tpl.php
@@ -187,31 +187,7 @@ t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
         <?php endif;?>
         <!--login start -->
         <?php if(!$logged_in):?>
-        <?php
-        //$block = module_invoke('block', 'block_view', 'tfk_login_form');
-        //print render($block);
-        ?>
-        <form action="/user" method="post" id="user_login" accept-charset="UTF-8">
-     	<div class="logincontent" id="login-container" >
-			<div id="login-header">Log in, Register or subscribe</div><div id="login-form-container">				
-				<input name="form_id" value="user_login" type="hidden">
-				<h2>Already Registered for subscriber-only teacher content? Log in now!</h2>
-				<div class="form-item form-type-textfield form-item-name"><label for="edit-name">Email:</label>&nbsp;<input class="form-text required" id="edit-name" maxlength="60" name="name" value="" type="text"></div>
-				<div class="form-item form-type-password form-item-pass"><label for="edit-pass">Password:</label>&nbsp;<input class="form-text required" id="edit-pass" maxlength="128" name="pass" value="" type="password"></div>
-				<div class="form-actions form-wrapper" id="edit-actions"><input class="form-submit" id="edit-submit" name="op" value="Log in" type="submit"></div>
-				<a href="https://subscription.timeforkids.com/storefront/universalForgotPassword.ep?magcode=TK">Forgot Password?</a>
-			</div>
-			<div id="message-box">
-				<div id="message-header">We've redesigned our site to be easier to use and added lots of new content and tools!</div>
-					<ul id="message-list">
-						<li>If you are a subscriber and you've already registered login here.</li>
-						<li>If you are a subscriber and this is your first visit to our site, click "Not yet registered?"</li>
-						<li>If you are not a subscriber, click "Want to subscribe?"</li>
-					</ul>
-				</div>
-			<div class="clearfix" id="login-footer"><a href="https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/tk_web_login.jsp&MSDDMOFF=AONF&MSDTRACK=TKSO">Not yet registered?</a> <a href="https://subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/site/tk-digital0711.html?link=1005016">Want to subscribe?</a></div>
-		</div>
-		</form>
+        <?php print render($page['content']); ?>
       <!--login end -->
 	  <?php endif;?>
 
