4fe428db80f48a391a80b492323fbd0ec6abc35d
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-18 20:03:08 +0000

    CR

--- docroot/sites/all/themes/tfk/templates/views-view-fields--news-archive.tpl.php
+++ docroot/sites/all/themes/tfk/templates/views-view-fields--news-archive.tpl.php
@@ -33,7 +33,7 @@ if(count($user_roles) == 1 && in_array('anonymous user',$user_roles)){
 ?>
 <?php foreach ($fields as $id => $field): ?>
 
-    <?php if($id != 'field_mini_lessons' && $id != 'field_related_articles'):?>
+    <?php if($id != 'field_mini_lessons' && $id != 'field_related_articles' && $id !='field_lrg_rect_image'):?>
       <?php if (!empty($field->separator)): ?>
         <?php print $field->separator; ?>
       <?php endif; ?>
