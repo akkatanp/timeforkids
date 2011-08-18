eaf22034dc92d50e2494f6753e6338f463277658
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-18 17:24:30 +0000

    CR

--- trunk/docroot/sites/all/themes/tfk/templates/tfk-news-minisite-related-articles.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/tfk-news-minisite-related-articles.tpl.php
@@ -1,7 +1,7 @@
 <?php if(!empty($articles)):?>
 
 TITLE OF THE MINISITE (HAS TO BE IN RED):<?php echo $articles[0]['minisite_title'];?><br />
-<?php print_r($articles);?>
+
 
     <?php foreach($articles as $article):?>
 
