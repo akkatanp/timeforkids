7c6cae4ba82df77e5d7c0b2c072f05d52812b518
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-18 17:13:54 +0000

    CR

--- docroot/sites/all/themes/tfk/templates/tfk-news-minisite-related-articles.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-news-minisite-related-articles.tpl.php
@@ -1,3 +1,11 @@
-<?php 
-print_r($articles);
-?>
+<?php if(!empty($articles)):?>
+
+TITLE OF THE MINISITE (HAS TO BE IN RED):<?php echo $articles[0]['minisite_title'];?>
+
+
+    <?php foreach($articles as $article):?>
+
+        <?php echo l($article['title'],'node/'.$article['nid']);?><br />
+
+    <?php endforeach; ?>
+<?php endif;?>
