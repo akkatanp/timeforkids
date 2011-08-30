a16acc2cada67dc48a760f7fa6724f7078315171
dmackinn <dmackinn@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-08-30 15:32:43 +0000

    fix for tfks426

--- trunk/docroot/sites/all/themes/tfk/js/atw-sightseeing.js
+++ trunk/docroot/sites/all/themes/tfk/js/atw-sightseeing.js
@@ -22,15 +22,18 @@
 				var locationTable = $(locationBody).find('table');
 				
 				var locationImg = $('<img/>');
+                                var locationName = $('<h1></h1>').text(locationTable.find('.boldBlack20 font').text().toLowerCase());
+				var locationText = $('<p></p>').text(locationTable.find('.storyText').eq(1).text());
 				locationImg.load(function() {
-					/*if (locationImg.width() < locationImg.height()) {
+					if (locationImg.width() < locationImg.height()) {
 						locationImg.css('float', 'left');
-					}*/
+                                                locationName.css('float', 'right');
+                                                locationText.css('float', 'right');
+					}
 				});
 				locationImg.attr('src',locationTable.find('img').eq(0).attr('src'));
 				
-				var locationName = $('<h1></h1>').text(locationTable.find('.boldBlack20 font').text().toLowerCase());
-				var locationText = $('<p></p>').text(locationTable.find('.storyText').eq(1).text());
+
 				
 				var locationDiv = $('<div></div>').attr('id', 'location-container').append(locationImg).append(locationName).append(locationText);
 				locationDiv.css({
