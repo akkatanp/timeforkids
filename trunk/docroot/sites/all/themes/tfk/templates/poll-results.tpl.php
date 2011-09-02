f444a2ead54314b1bb5bd203cafcf442f6ba3514
voitenkos <voitenkos@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-09-02 15:46:19 +0000

    polls

--- trunk/docroot/sites/all/themes/tfk/templates/poll-results.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/poll-results.tpl.php
@@ -19,6 +19,40 @@
  */
 ?>
 <div class="poll">
+  <?php print $results; ?>
+  <div class="total">
+    <?php print t('Total votes: @votes', array('@votes' => $votes)); ?>
+  </div>
+  <?php if (!empty($cancel_form)): ?>
+    <?php print $cancel_form; ?>
+  <?php endif; ?>
+</div>
+
+
+
+
+<?php
+// just in case  keeping old tpl 
+if('old' == 'new'):
+?>
+/**
+ * @file
+ * Default theme implementation to display the poll results in a block.
+ *
+ * Variables available:
+ * - $title: The title of the poll.
+ * - $results: The results of the poll.
+ * - $votes: The total results in the poll.
+ * - $links: Links in the poll.
+ * - $nid: The nid of the poll
+ * - $cancel_form: A form to cancel the user's vote, if allowed.
+ * - $raw_links: The raw array of links.
+ * - $vote: The choice number of the current user's vote.
+ *
+ * @see template_preprocess_poll_results()
+ */
+?>
+<div class="poll">
   <h3 class="title"><?php print $title ?></h3>
   <?php print $results ?>
 </div>
@@ -32,4 +66,6 @@
     <?php print $cancel_form; ?>
   <?php endif; ?>
 </div>
--->
+-->    
+
+<?php endif;?>
