f06d4121302e72ed36240910efa088f482845ecc
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-09-15 16:42:29 +0000

    Push trunk to test as tag tags/2011-09-15

---
+++ tags/2011-09-15/docroot/sites/all/themes/tfk/templates/tfk-news-taxonomy-links.tpl.php
@@ -0,0 +1,12 @@
+<?php echo l('NEWS ARCHIVE','news-archive');?>
+
+<ul class="menu">
+  <li><?php echo l('All News','news-archive/all',array('attributes' => array('class'=>array($term['class']))));?></li>
+<?php foreach($terms_with_articles as $term): ?>
+  <li><?php echo l($term['term_name'],'news-archive/'.strtolower($term['term_name']),array('attributes' => array('class'=>array($term['class']))));?></li>
+<?php endforeach; ?>
+  
+  <?php if($display_poll_link == 1):?>
+    <li><?php echo l('Polls','news-archive/polls',array('attributes' => array('class'=>'')));?>
+  <?php endif; ?>
+</ul>
