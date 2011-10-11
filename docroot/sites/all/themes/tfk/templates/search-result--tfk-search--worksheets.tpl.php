8aaff32e8d28a1d715026369b65ec973a172e8b9
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-10-11 16:38:59 +0000

    Add PDF download link to Printables SERP.

--- docroot/sites/all/themes/tfk/templates/search-result--tfk-search--worksheets.tpl.php
+++ docroot/sites/all/themes/tfk/templates/search-result--tfk-search--worksheets.tpl.php
@@ -123,6 +123,10 @@
           	<span class="search-result-download-link <?php if(isset($cont_type_class)): ?><?php print $cont_type_class; ?><?php endif; ?>"><?php print $tfk_worksheet_pdf_link; ?></span>
           <?php endif; ?>
           
+          <?php if(isset($tfk_pdf_link)): ?>
+          	<span class="search-result-download-link <?php if(isset($cont_type_class)): ?><?php print $cont_type_class; ?><? endif; ?>"><?php print $tfk_pdf_link; ?></span>
+          <?php endif; ?>
+          
           <?php if($favorites_flag_link): ?>
           	<?php print $favorites_flag_link; ?>
           <?php endif; ?>
