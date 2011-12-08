8019261a6e512ea7b5ba8ca9a2f6c9f0276c8a35
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2011-12-08 15:43:27 +0000

    Remove scale="tofit" parameter from video embed. Hardcode height to 275 pixels.

--- trunk/docroot/sites/all/modules/contrib/video/theme/video-play-quicktime.tpl.php
+++ trunk/docroot/sites/all/modules/contrib/video/theme/video-play-quicktime.tpl.php
@@ -2,19 +2,19 @@
 /*
  * @file
  * Theme file to handle quicktime output.
- * 
+ *
  * Variables passed.
  * $video is the video object.
  * $node is the node object.
  *
- * 
+ *
  * <script type="text/javascript" src="/sites/all/libraries/quicktime/AC_QuickTime.js"></script>
  * <script type="text/javascript">
  *   QT_WriteOBJECT('<?php print $video->files->{$video->player}->url; ?>' , '<?php print $video->width; ?>', '<?php print $video->height; ?>' , '');
  * </script>
  */
 ?>
-<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab#version=7,3,0,0"  width="<?php print $video->player_width; ?>" height="<?php print $video->player_height; ?>">
+<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab#version=7,3,0,0"  width="<?php print $video->player_width; ?>" height="275">
   <param name="src" value="<?php print $video->files->{$video->player}->url;?>" />
   <param name="controller" value="true" />
   <param name="scale" value="tofit" />
@@ -22,10 +22,9 @@
   <param name="pluginurl" value="http://www.apple.com/quicktime/download/" />
   <embed src="<?php print $video->files->{$video->player}->url; ?>"
          type="video/quicktime"
-         scale="tofit"
          pluginspage="http://www.apple.com/quicktime/download/"
          width="<?php print $video->player_width; ?>"
-         height="<?php print $video->player_height; ?>"
+         height="275"
          autostart="<?php print $video->autoplay ? 'true' : 'false'; ?>"
          controller="true" >
   </embed>
