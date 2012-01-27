2e1f70eb2b4535696f36b89cb29f865267898315
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-01-27 08:44:52 +0000

    sam.lerner@acquia - 15066-30759 - Fix for settings.php for memcache Patch for revision 4446 -> 4447.

--- trunk/docroot/sites/default/settings.php
+++ trunk/docroot/sites/default/settings.php
@@ -455,7 +455,7 @@ $conf['cache_inc'] = './sites/all/modules/contrib/memcache/memcache.inc';
 //$conf['session_inc'] = './sites/all/modules/contrib/memcache/memcache-session.inc';
 
 include_once('./includes/cache.inc');
-include_once('./sites/all/modules/memcache/memcache.inc');
+include_once('./sites/all/modules/contrib/memcache/memcache.inc');
 $conf['cache_default_class'] = 'MemCacheDrupal'; 
 
 /**
