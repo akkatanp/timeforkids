dcde18c2f0a93d661ac3abee525f5e31dd6dad2c
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-02-08 22:02:10 +0000

    Changes to login page.

--- trunk/docroot/sites/all/themes/tfk/templates/tfk-user-login.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/tfk-user-login.tpl.php
@@ -2,26 +2,31 @@
 $form = array_shift($variables);
 ?>
 <div class="logincontent" id="login-container" >
-	<div id="login-header">Log in, Register or subscribe</div><div id="login-form-container">
+	<div id="login-header">Current subscribers log in/register for timeforkids.com</div><div id="login-form-container">
 		<?php
 			if(isset($form['form_build_id'])) {
 				print render($form['form_build_id']);
 			}
 		?>
 		<input name="form_id" value="user_login" type="hidden">
-		<h2>Already Registered for subscriber-only teacher content? Log in now!</h2>
+		<h2>Registered Users Log In</h2>
 		<div class="form-item form-type-textfield form-item-name"><label for="edit-name">Email:</label>&nbsp;<input class="form-text required" id="edit-name" maxlength="60" name="name" value="" type="text"></div>
 		<div class="form-item form-type-password form-item-pass"><label for="edit-pass">Password:</label>&nbsp;<input class="form-text required" id="edit-pass" maxlength="128" name="pass" value="" type="password"></div>
 		<div class="form-actions form-wrapper" id="edit-actions"><input class="form-submit" id="edit-submit" name="op" value="Log in" type="submit"></div>
 		<a href="https://subscription.timeforkids.com/storefront/universalForgotPassword.ep?magcode=TK">Forgot Password?</a>
 	</div>
+	<div>
+	<div class="bold">Register Now for FREE<br />Subscriber Benefits</div>
+	<div class="form-actions form-wrapper"><a href="https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/tk_web_login.jsp&MSDDMOFF=AONF&MSDTRACK=TKSO"><button class="form-submit" value="Register Now!" type="button">Register Now!</button></a></div>
+	</div>
 	<div id="message-box">
-		<div id="message-header">We've redesigned our site to be easier to use and added lots of new content and tools!</div>
+		<div id="message-header">Do it now to get all this:</div>
 			<ul id="message-list">
-				<li>If you are a subscriber and you've already registered login here.</li>
-				<li>If you are a subscriber and this is your first visit to our site, click "Not yet registered?"</li>
-				<li>If you are not a subscriber, click "Want to subscribe?"</li>
+				<li>Access to Interactive Digital Editions</li>
+				<li>Online Archives of Past Lessons & Teachers' Guides</li>
+				<li>Interactive Teacher Community</li>
 			</ul>
 		</div>
-	<div class="clearfix" id="login-footer"><a href="https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/tk_web_login.jsp&MSDDMOFF=AONF&MSDTRACK=TKSO">Not yet registered?</a> <a href="https://subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1005016.html">Want to subscribe?</a></div>
+	<div class="clearfix" id="login-footer"><span class="bold">Not Yet A Subscriber?</span> <a href="https://subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1005016.html">Click here to subscribe</a> </div>
+
 </div>
