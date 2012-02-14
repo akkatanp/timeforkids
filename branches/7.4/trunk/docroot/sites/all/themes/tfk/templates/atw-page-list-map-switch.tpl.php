898cd6b3a865e20f2efee4509889b7ac7c743bfc
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-02-14 15:32:21 +0000

    Delete 7.4 branch, it was never used.

--- branches/7.4/trunk/docroot/sites/all/themes/tfk/templates/atw-page-list-map-switch.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="atw-switcher-text">Destinations By World Region</div>
-<div class="atw-switcher-links">
-<?php echo l('List View','around-the-world',array('attributes' => array('class' => array('list-view'))));?><br/>
-<?php echo l('Map View','around-the-world/map',array('attributes' => array('class' => array('map-view'))));?><br/>
-</div>
