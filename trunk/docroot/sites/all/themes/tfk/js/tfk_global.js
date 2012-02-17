5fd47f4467bea892bb10ad8f1265e2b262f6469f
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-02-17 20:20:13 +0000

    re #558 re #398 Rewrite JS to use Drupal behaviors and contexts.

--- trunk/docroot/sites/all/themes/tfk/js/tfk_global.js
+++ trunk/docroot/sites/all/themes/tfk/js/tfk_global.js
@@ -42,39 +42,39 @@
 	
 	
 	$.fn.loginBox = function(context) {
-		$(this, context).click(function(e) {
-			e.preventDefault();
-			createLightBox();
-		});
-		
-		var createLightBox = function() {
-			$('body', context).css('overflow', 'hidden');
-			
-			var mask = $('<div></div>', context).attr('id', 'mask').css({
-				'height': $(window).height() + 'px',
-				'top': $(window).scrollTop() + 'px'
-			}).appendTo($(document.body));
-			
-			var lightBox = $('<div></div>', context).attr('id', 'lightbox').css({
-				'top': (($(window).height() / 2) - 185) + $(window).scrollTop() + 'px',
-				'left': (($(window).width() / 2) - 350) + 'px'
-			}).appendTo($(document.body));
-			
-			var loginForm = $('#user-login', context).clone();
-			loginForm.removeAttr('id');
-			loginForm.attr('id', 'user_login');
-			loginForm.appendTo(lightBox);
-			
-			$("#user_login", context).jCryption();
-			
-			var closeButton = $('#close-button', context).click(function(e) {
+	  
+	  // Create lightbox when user clicks link.
+	  $(this, context).click(function(e) {
+      e.preventDefault();
+
+      $('body', context).css('overflow', 'hidden');
+      
+      var mask = $('<div></div>', context).attr('id', 'mask').css({
+        'height': $(window).height() + 'px',
+        'top': $(window).scrollTop() + 'px'
+      }).appendTo($(document.body));
+      
+      var lightBox = $('<div></div>', context).attr('id', 'lightbox').css({
+        'top': (($(window).height() / 2) - 185) + $(window).scrollTop() + 'px',
+        'left': (($(window).width() / 2) - 350) + 'px'
+      }).appendTo($(document.body));
+      
+      var loginForm = $('#user-login', context).clone();
+      loginForm.removeAttr('id');
+      loginForm.attr('id', 'user_login');
+      loginForm.appendTo(lightBox);
+      
+      $("#user_login", context).jCryption();
+      
+      var closeButton = $('#close-button', context).click(function(e) {
         e.preventDefault();
         $('#lightbox', context).remove();
         $('#mask', context).remove();
         $('body', context).css('overflow', 'auto');
       });
-			
-		};
+      
+	  });	  
+		
 		
 	};	
 })(jQuery);
