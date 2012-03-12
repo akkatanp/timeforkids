0518fc7f9ee28f1066f19694fee6b11c1b576d66
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-03-12 17:27:12 +0000

    re #574 Open digital link in separate window.

--- trunk/docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
@@ -1,17 +1,17 @@
 <?php
 /*
 * This page is used to login from timeforkids.com to the digital product
-* without having to re-enter one's login and password. Its done via post 
+* without having to re-enter one's login and password. Its done via post
 * of the LUCIE auth token and two variables.
 */
 
-/* 
+/*
 *	Set the post fields to sent to the digital product page
 */
 global $user;
 $cgiToken = ($user->uid ? $_COOKIE['CGI-token'] : '');
 ?>
-<form id="cgi-redirect" action="https://app.timeforkidsdigital.com/cgiAuth.php" method="post">
+<form id="cgi-redirect" action="https://app.timeforkidsdigital.com/cgiAuth.php" method="post" target="_blank">
 	<input type="hidden" name="CGI-Token-Exp" value="2592000000" />
 	<input type="hidden" name="CGI-Remember-Me" value="0" />
 	<input type="hidden" name="CGI-Token" value="<?php echo $cgiToken; ?>" />
