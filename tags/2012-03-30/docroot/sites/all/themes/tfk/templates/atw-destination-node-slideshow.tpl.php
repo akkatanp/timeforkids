6362a99254dc6b99affc1a3ec3a12224683f30c0
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-03-30 20:35:29 +0000

    Push trunk to test as tag tags/2012-03-30

---
+++ tags/2012-03-30/docroot/sites/all/themes/tfk/templates/atw-destination-node-slideshow.tpl.php
@@ -0,0 +1,24 @@
+<?php if(isset($images_data)):?>
+	<div id="slideshow">
+		<div class="header"></div>
+		<div class="outer">
+			<div class="inner">
+			<?php foreach($images_data as $image_k => $image_obj): ?>
+				<img src="<?php echo $image_obj['image_path']; ?>" />
+			<?php endforeach;?>
+			</div>
+		</div>
+		<div class="footer">
+			<div class="title-container">
+			<?php foreach($images_data as $image_k => $image_obj): ?>
+				 <div class="title" style="display: none;"><?php echo $image_obj['image_title']; ?></div>
+			<?php endforeach;?>
+			</div>
+		</div>
+	</div>
+<div class="foot-credit">
+                 <?php foreach($images_data as $image_k => $image_obj): ?>
+                   <div class="creditval" style="display: none;"><?php echo $image_obj['image_credit']; ?></div>
+                 <?php endforeach;?>
+                </div>
+<?php endif;?>
