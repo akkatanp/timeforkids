da7c863169423c9a34265228d8ac950cb8f54b0a
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-05-29 15:51:33 +0000

    Point LUCIE single-sign on calls to new QA URL.

--- docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-profile-settings.tpl.php
@@ -6,9 +6,9 @@
      <strong>Profile</strong>
    </div>
    <div class="settingslink">
-    <a href="https://subscription.timeforkids.com/storefront/universalForgotPassword.ep?magcode=TK">Change Password</a>
+    <a href="https://qa-subscription.timeforkids.com/storefront/universalForgotPassword.ep?magcode=TK">Change Password</a>
    </div>
    <div class="settingslink">
-     <a href="https://subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/site/tk-digital0711.html?link=1005016">Subscription</a>
+     <a href="https://qa-subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/site/tk-digital0711.html?link=1005016">Subscription</a>
    </div>
 </div>
