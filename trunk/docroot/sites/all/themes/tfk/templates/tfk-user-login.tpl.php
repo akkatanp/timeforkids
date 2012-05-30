2a3e8e35acf798bc7e7f8cff01ac630a21934ff5
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-05-30 00:12:11 +0000

    - Cleanup user login template: use template variables, add dynamic subscription URL. - Remove unused theming function. - Pre-render form build ID for login from tfk_helper_block_view(); add result to user login form render array so it can get copied over to the template. - Remove hook_preprocess_tfk_login; substitute with hook_theme_registry_alter; inject NULL rendered_build_id variable into theme hook "user_login", so the build id variable gets copied from the form render array into the template during hook_theme's execution.

--- trunk/docroot/sites/all/themes/tfk/templates/tfk-user-login.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/tfk-user-login.tpl.php
@@ -1,17 +1,12 @@
-<?php
-$form = array_shift($variables);
-?>
 <div class="logincontent" id="login-container" >
   <div id="login-header">Current subscribers log in/register for timeforkids.com<?php if($close_button): ?><a id="close-button" class="close-button">&nbsp;</a><?php endif; ?></div><div id="login-form-container">
-    <?php if(isset($form['form_build_id'])): ?>
-       <?php print render($form['form_build_id']); ?>
-    <?php endif; ?>
+    <?php print $rendered_build_id; ?>
     <input name="form_id" value="user_login" type="hidden">
     <h2>Registered Users Log In</h2>
     <div class="form-item form-type-textfield form-item-name"><label for="edit-name">Email:</label>&nbsp;<input class="form-text required" id="edit-name" maxlength="60" name="name" value="" type="text"></div>
     <div class="form-item form-type-password form-item-pass"><label for="edit-pass">Password:</label>&nbsp;<input class="form-text required" id="edit-pass" maxlength="128" name="pass" value="" type="password"></div>
     <div class="form-actions form-wrapper" id="edit-actions"><input class="form-submit" id="edit-submit" name="op" value="Log in" type="submit"></div>
-    <a href="https://<?php print $variables['lucie_callback_domain']; ?>/storefront/universalForgotPassword.ep?magcode=TK">Forgot Password?</a>
+    <a href="https://<?php print $lucie_callback_domain; ?>/storefront/universalForgotPassword.ep?magcode=TK">Forgot Password?</a>
   </div>
   <div>
   <div class="bold">Register Now for FREE<br />Subscriber Benefits</div>
@@ -25,6 +20,6 @@ $form = array_shift($variables);
         <li>Interactive Teacher Community</li>
       </ul>
     </div>
-  <div class="clearfix" id="login-footer"><span class="bold">Not Yet A Subscriber?</span> <a href="https://<?php print $variables['lucie_callback_domain']; ?>/storefront/subscribe-to-time-for-kids/link/1005016.html">Click here to subscribe</a> </div><div style="float: right; font-size: 9px;">Website Login Page</div>
+  <div class="clearfix" id="login-footer"><span class="bold">Not Yet A Subscriber?</span> <a href="https://<?php print $lucie_callback_domain; ?>/storefront/subscribe-to-time-for-kids/link/1005016.html">Click here to subscribe</a> </div><div style="float: right; font-size: 9px;">Website Login Page</div>
 
 </div>
