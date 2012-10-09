2b47485e961239d9d91bff82e7f6cea83468c0d8
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-10-09 16:22:37 +0000

    tfks696 Add polldaddy/% callback for standalone external polls.

--- trunk/docroot/sites/all/modules/custom/tfk_polls/external-poll.tpl.php
+++ trunk/docroot/sites/all/modules/custom/tfk_polls/external-poll.tpl.php
@@ -1,7 +1,7 @@
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
-    <div class="tfkpollheading">TFK Poll</div>
+    <?php if(!$variables['standalone']): ?><div class="tfkpollheading">TFK Poll</div><?php endif; ?>
     <!-- <div class="tfkpolltitle"><?php print $variables['title'];?></div>-->
     <div class="tfkpollimage">
       <script type="text/javascript" charset="utf-8" src="http://static.polldaddy.com/p/<?php print $variables['poll-id']; ?>.js"></script>
