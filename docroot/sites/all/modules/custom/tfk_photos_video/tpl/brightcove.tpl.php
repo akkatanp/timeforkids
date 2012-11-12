2264ba9356fc74d3d21e6f5925061af0c47c1fb9
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-11-12 20:55:51 +0000

    re tfks699 Modify template.

--- docroot/sites/all/modules/custom/tfk_photos_video/tpl/brightcove.tpl.php
+++ docroot/sites/all/modules/custom/tfk_photos_video/tpl/brightcove.tpl.php
@@ -1,16 +1,33 @@
-<script src="http://admin.brightcove.com/js/BrightcoveExperiences.js" type="text/javascript"></script>
-<script src="http://admin.brightcove.com/js/APIModules_all.js"></script>
-<object id="myExperience" class="BrightcoveExperience">
+<!-- Start of Brightcove Player -->
+
+<div style="display:none">
+</div>
+
+<!--
+By use of this code snippet, I agree to the Brightcove Publisher T and C
+found at https://accounts.brightcove.com/en/terms-and-conditions/.
+-->
+
+<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+
+<object id="myExperience1958986629001" class="BrightcoveExperience">
   <param name="bgcolor" value="#FFFFFF" />
   <param name="width" value="480" />
-  <param name="height" value="350" />
-  <param name="playerID" value="<?php print $variables['player_id']; ?>" />
-  <param name="videoID" value="<?php print $variables['video_id']; ?>" />
+  <param name="height" value="270" />
+  <param name="playerID" value="<?php print $variables['player_id']; ?>" /><!-- 1873162655001 -->
+  <param name="playerKey" value="AQ~~,AAABs_kuylk~,vRYixSFTu-K4sztp2goZaSRYuNk4hAZU" />
   <param name="isVid" value="true" />
-  <param name="optimizedContentLoad" value="true" />
-  <param name="externalAds" value="false" />
-  <param name="continuousPlay" value="false" />
-  <param name="autoStart" value="false" />
-  <param name="allowScriptAccess" value="always" />
+  <param name="isUI" value="true" />
+  <param name="dynamicStreaming" value="true" />
+
+  <param name="@videoPlayer" value="<?php print $variables['video_id']; ?>" /><!--1958986629001-->
 </object>
+
+<!--
+This script tag will cause the Brightcove Players defined above it to be created as soon
+as the line is read by the browser. If you wish to have the player instantiated only after
+the rest of the HTML is processed and the page load is complete, remove the line.
+-->
 <script type="text/javascript">brightcove.createExperiences();</script>
+
+<!-- End of Brightcove Player -->
