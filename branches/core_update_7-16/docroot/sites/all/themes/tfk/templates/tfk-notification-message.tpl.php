3ff7611ea11b53137c6f241bf86e7f8dc913c38a
rallen <rallen@ef34a74e-757f-47cd-94cb-817147fcdc54>
2012-11-28 15:14:00 +0000

    Delete branch.

--- branches/core_update_7-16/docroot/sites/all/themes/tfk/templates/tfk-notification-message.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if(count($notification_data) > 0):?>
-<div class="notification">
-	<h1><?php print $notification_data['notification_title'];?></h1>
-	<?php print $notification_data['notification_body'];?>
-	<a href="<?php print $notification_data['notification_link'];?>">Click For More Information</a>
-	<a href="#" id="hide-notification" rel="<?php echo url('notification/hide/'.$notification_data['notification_nid']);?>">Hide This</a>
-</div>
-<?php endif;?>
