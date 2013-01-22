536bc6f3362e692e883bcf52c90fb669d0a67d2f
dmackinn <dmackinn@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-01-22 21:30:47 +0000

    Removed invisible characters from xid urls

--- docroot/sites/all/themes/tfk/js/store.js
+++ docroot/sites/all/themes/tfk/js/store.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
             var xid = "";
             var newHref = "";
             //Add more links/xids here
-            xidMap['wetelp23'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014068.html";
-            xidMap['wetelp24'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014069.html";
-            xidMap['wetelp25'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014070.html";
-            xidMap['wetelp26'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014071.html";
-            xidMap['wetelp27'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014072.html";
-            xidMap['wetelp28'] = "https://subscription​.timeforkids.com/sto​refront/subscribe-to​-time-for-kids/link/​1014073.html";
+            xidMap['wetelp23'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014068.html";
+            xidMap['wetelp24'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014069.html";
+            xidMap['wetelp25'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014070.html";
+            xidMap['wetelp26'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014071.html";
+            xidMap['wetelp27'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014072.html";
+            xidMap['wetelp28'] = "https://subscription​.timeforkids.com/storefront/subscribe-to-time-for-kids/link/1014073.html";
             parameters = window.location.search.substring(1);
             if (parameters != "") {
                 parameterArray = parameters.split("&");
