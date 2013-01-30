f0ecfd0606df5ce08a3b4520032f9f30be9bb2ab
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-01-30 19:51:24 +0000

    Tag tags/2013-01-30 generated automatically from branches/core_update_7-18_modified

---
+++ tags/2013-01-30/docroot/sites/all/themes/tfk/templates/tfk-news-taxonomy-links.tpl.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($term['class'])) {
+  $attributes = array('attributes' => array('class' => array($term['class'])));
+} else {
+  $attributes = array();
+}
+?>
+<?php echo l('NEWS ARCHIVE','news-archive');?>
+
+<ul class="menu">
+  <li><?php echo l('All News','news-archive/all',array('attributes' => array('class'=>'')));?></li>
+<?php foreach($terms_with_articles as $term): ?>
+  <li><?php echo l($term['term_name'], 'news-archive/'. strtolower($term['term_name']), $attributes);?></li>
+<?php endforeach; ?>
+  
+  <?php if($display_poll_link == 1):?>
+    <li><?php echo l('Polls','news-archive/polls',array('attributes' => array('class'=>'')));?>
+  <?php endif; ?>
+</ul>
