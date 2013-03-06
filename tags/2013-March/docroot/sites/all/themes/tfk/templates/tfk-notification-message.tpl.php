3bf9d686465aa1d65371236a813201634cf6402e
srivaths <srivaths@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-03-06 20:35:40 +0000

    tfk-521

--- tags/2013-March/docroot/sites/all/themes/tfk/templates/tfk-notification-message.tpl.php
+++ tags/2013-March/docroot/sites/all/themes/tfk/templates/tfk-notification-message.tpl.php
@@ -2,7 +2,9 @@
 <div class="notification">
 	<h1><?php print $notification_data['notification_title'];?></h1>
 	<?php print $notification_data['notification_body'];?>
-	<a href="<?php print $notification_data['notification_link'];?>">Click For More Information</a>
+	<?php if (isset($notification_data['notification_link'])):?>
+		<a href="<?php print $notification_data['notification_link'];?>">Click For More Information</a>
+	<?php endif;?>
 	<a href="#" id="hide-notification" rel="<?php echo url('notification/hide/'.$notification_data['notification_nid']);?>">Hide This</a>
 </div>
 <?php endif;?>
