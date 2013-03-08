c19887357c58bd1f75008f2285a671a9a77c0e82
srivaths <srivaths@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-03-08 19:48:04 +0000

    tfk-744

--- tags/2013-March/docroot/sites/all/themes/tfk/templates/node.tpl.php
+++ tags/2013-March/docroot/sites/all/themes/tfk/templates/node.tpl.php
@@ -125,7 +125,7 @@
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php if($type == 'video'): ?>
-     <span class="date"><?php print $variables['field_date'][0]; ?></span>
+     <span class="image-date"><?php print $variables['field_date'][0]; ?> Aug 17</span>
     <?php endif; ?>
   
     <?php if (isset($unpublished) && $unpublished): ?>
