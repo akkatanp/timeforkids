07802045f133d65c179bffc67a77a0fcf70cdb1d
nbailly <nbailly@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-05-06 14:23:37 +0000

    Update to views-7.x-3.7

--- branches/2013-May-Updates/docroot/sites/all/modules/contrib/views/theme/views-exposed-form.tpl.php
+++ branches/2013-May-Updates/docroot/sites/all/modules/contrib/views/theme/views-exposed-form.tpl.php
@@ -1,7 +1,7 @@
 <?php
+
 /**
- * @file views-exposed-form.tpl.php
- *
+ * @file
  * This template handles the layout of the views exposed filter form.
  *
  * Variables available:
@@ -29,7 +29,7 @@
 <div class="views-exposed-form">
   <div class="views-exposed-widgets clearfix">
     <?php foreach ($widgets as $id => $widget): ?>
-      <div class="views-exposed-widget views-widget-<?php print $id; ?>">
+      <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
         <?php if (!empty($widget->label)): ?>
           <label for="<?php print $widget->id; ?>">
             <?php print $widget->label; ?>
@@ -43,6 +43,11 @@
         <div class="views-widget">
           <?php print $widget->widget; ?>
         </div>
+        <?php if (!empty($widget->description)): ?>
+          <div class="description">
+            <?php print $widget->description; ?>
+          </div>
+        <?php endif; ?>
       </div>
     <?php endforeach; ?>
     <?php if (!empty($sort_by)): ?>
