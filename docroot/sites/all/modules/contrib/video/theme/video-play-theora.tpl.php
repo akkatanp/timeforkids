849978dbfd85ad5ec710846c55f6aa5bb9cfd968
nbailly <nbailly@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-05-09 13:11:51 +0000

    Revert back to video 7.x-1.0-alpha2

--- docroot/sites/all/modules/contrib/video/theme/video-play-theora.tpl.php
+++ docroot/sites/all/modules/contrib/video/theme/video-play-theora.tpl.php
@@ -1,20 +1,16 @@
-<?php
-/**
+<?php 
+/*
  * @file
  * Theme file to handle ogg theora output.
- *
- * Variables passed:
- * $item
- * $width
- * $height
- * $autoplay
- * $autobuffering
+ * 
+ * Variables passed.
+ * $video is the video object.
+ * $node is the node object.
+ * 
  */
-
-$url = check_plain(file_create_url($item['playablefiles'][0]->uri));
-?>
-<applet code="com.fluendo.player.Cortado.class" archive="http://theora.org/cortado.jar" width="<?php print $width; ?>" height="<?php print $height; ?>">
-  <param name="url" value="<?php print $url; ?>" />
+?> 
+<applet=code="com.fluendo.player.Cortado.class" archive="<?php print $video->theora_player; ?>" width="<?php print $video->player_width; ?>" height="<?php print $video->player_height; ?>">
+  <param name="url" value="<?php print $video->files->{$video->player}->url; ?>" />
   <param name="local" value="false" />
   <param name="mode" value="zero" />
   <param name="keepaspect" value="true" />
@@ -22,8 +18,5 @@ $url = check_plain(file_create_url($item['playablefiles'][0]->uri));
   <param name="audio" value="true" />
   <param name="seekable" value="true" />
   <param name="bufferSize" value="200" />
-  <param name="autoPlay" value="<?php print $autoplay ? 'true' : 'false'; ?>" />
-  <param name="showStatus" value="auto" />
-  <param name="showSpeaker" value="true" />
-  <?php print t('No video? Get the !plugin', array('!plugin' => l(t('Cortado plugin'), url('http://www.theora.org/cortado/')))); ?>
+  <?php print t('No video?  Get the Latest Cortado !plugin', array('!plugin' => l(t('Plugin'), 'http://www.theora.org/cortado/'))); ?>
 </applet>
