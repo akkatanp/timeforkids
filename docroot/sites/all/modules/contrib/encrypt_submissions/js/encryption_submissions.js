21657271979385913e90fe15859e8a80a483f21a
nbailly <nbailly@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-05-10 12:29:52 +0000

    Update to encrypt_submissions 7.x-1.3

--- docroot/sites/all/modules/contrib/encrypt_submissions/js/encryption_submissions.js
+++ docroot/sites/all/modules/contrib/encrypt_submissions/js/encryption_submissions.js
@@ -1,37 +1,47 @@
-
-
 /**
  * This will run at start up, and attach the jCryption behavior to 
  * all of the forms specified in Drupal.settings.encrypt_submissions.encryptForms
  *
  **/
-Drupal.behaviors.encrypt_submissions_startup= {
+var jq = jQuery;
+Drupal.behaviors.encrypt_submissions_startup = {
   attach: function (context, settings) {
-    
-    // Only do this once...
-    jQuery.jCryption.defaultOptions.getKeysURL = settings.encrypt_submissions.baseUrl + "/encrypt-submissions/generate-keypair";
-    
-    
-    // Loop through all the possible forms we should be affecting...
-    
-    for (var form_id in Drupal.settings.encrypt_submissions.encryptForms) {
-      var dom_form_id = Drupal.settings.encrypt_submissions.encryptForms[form_id];
+    // We need the ?q= syntax for people with clean URLs turned off
+    jq.jCryption.defaultOptions.getKeysURL
+      = settings.encrypt_submissions.baseUrl
+      + "?q=encrypt-submissions/generate-keypair&esrnd=" + encryptSubmissionsRandomString();
 
-      jQuery("#encrypt_submissions_status_" + dom_form_id).hide();
-      
-      jQuery("#" + dom_form_id + ":input").removeAttr("disabled");
-      
-      jQuery("#" + dom_form_id).jCryption();
-      
-      // Add an "on submit" event for this form, where we will
-      // make the Encrypting status message show up.
-      jQuery("#" + dom_form_id + " .form-submit").click({"dom_form_id" : dom_form_id}, function (e) {
-        var dfid = e.data.dom_form_id;
-        jQuery("#encrypt_submissions_status_" + dfid).show();         
-      });
-      
-    }
-      
- }
+    // Loop through all the possible forms we should be affecting...
+    jq.each(
+      Drupal.settings.encrypt_submissions.encryptForms,
+      function(form_id, el_id) { 
+        jq("#encrypt_submissions_status_" + el_id).hide();
+        jq("#" + el_id + ":input").removeAttr("disabled");
+        jq("#" + el_id).jCryption({
+          beforeEncryption: function() {
+            // make the Encrypting status message show up.
+            jq("#encrypt_submissions_status_" + el_id).show();
+            return true;
+          }
+        });
+      }
+    );
+  }
 };
-      
+
+
+
+/**
+ * Returns a X-length string of random letters and numbers.
+ * Used to construct a random URL which circumvents Drupal's caching system.
+ **/
+function encryptSubmissionsRandomString() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var length = 20;
+	var randomstring = '';
+	for (var i = 0; i < length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	return randomstring;
+}
