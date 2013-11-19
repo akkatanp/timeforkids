c5807c0f86d8a8d1b456f02fa4cac35af65a5303
acquia-fields-webnode <acquia-fields-webnode@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-11-19 18:28:04 +0000

    2013-November-Release
    Tag tags/2013-11-19 generated automatically from trunk

---
+++ tags/2013-11-19/docroot/sites/all/themes/tfk/templates/block--block--37.tpl.php
@@ -0,0 +1,12 @@
+<div class="current-issue-widget-redux">
+    <div class="title">TFK Magazine</div>
+    <div class="magazines">
+    <?php
+        print views_embed_view('current_issue_widget','gk1');
+        print views_embed_view('current_issue_widget','g2');
+        print views_embed_view('current_issue_widget','g34');
+        print views_embed_view('current_issue_widget','g56');
+    ?>
+    </div>
+    <div class="subscribe-now-redux"><a href="https://subscription.timeforkids.com/storefront/subscribe-to-time-for-kids/site/tk-sy1314onlineoffer0213.html?link=1005015" class="external">Subscribe Now</a></div>
+</div>
