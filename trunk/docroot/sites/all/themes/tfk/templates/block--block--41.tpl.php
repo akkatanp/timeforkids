5a866abb8c8d9f8b530f00bb90fd0537c9017a03
hhope <hhope@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-12-04 22:49:38 +0000

    Change Request: Remove 'external' class from magazine links.

--- trunk/docroot/sites/all/themes/tfk/templates/block--block--41.tpl.php
+++ trunk/docroot/sites/all/themes/tfk/templates/block--block--41.tpl.php
@@ -8,5 +8,5 @@
         print views_embed_view('current_issue_widget','g56');
     ?>
     </div>
-    <div class="subscribe-now-redux"><a href="current-issue-subscribe-now" class="external">Subscribe Now</a></div>
+    <div class="subscribe-now-redux"><a href="current-issue-subscribe-now">Subscribe Now</a></div>
 </div>
