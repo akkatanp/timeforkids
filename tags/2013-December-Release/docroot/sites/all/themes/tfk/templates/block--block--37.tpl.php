c396091e260c64aa96a1e5628c6456191fe38fe8
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2013-12-19 16:31:16 +0000

    2013-December-Release

---
+++ tags/2013-December-Release/docroot/sites/all/themes/tfk/templates/block--block--37.tpl.php
@@ -0,0 +1,12 @@
+<div class="current-issue-widget-redux">
+    <div class="title">TFK Magazine</div>
+    <div class="magazines">
+    <?php
+        print views_embed_view('current_issue_widget','gk1');
+        print views_embed_view('current_issue_widget','g2');
+        print views_embed_view('current_issue_widget','g34');
+        print views_embed_view('current_issue_widget','g56');
+    ?>
+    </div>
+    <div class="subscribe-now-redux"><a href="current-issue-subscribe-now">Subscribe Now</a></div>
+</div>
