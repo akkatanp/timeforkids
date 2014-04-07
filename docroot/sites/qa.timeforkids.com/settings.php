bc3ac58937189c5e652a7b105d2db6f971b342f9
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-04-07 21:00:34 +0000

    2014 April Release

--- docroot/sites/qa.timeforkids.com/settings.php
+++ docroot/sites/qa.timeforkids.com/settings.php
@@ -195,7 +195,7 @@ ini_set('session.cookie_lifetime', 0);
  * HTTP proxy, and bypass the reverse proxy if one is used) in order to avoid
  * getting cached pages from the proxy.
  */
-# $conf['omit_vary_cookie'] = TRUE;
+ $conf['omit_vary_cookie'] = TRUE;
 
 /**
  * CSS/JS aggregated file gzip compression:
