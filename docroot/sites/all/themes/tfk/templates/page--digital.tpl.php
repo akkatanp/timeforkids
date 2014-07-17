0ac9f40b4fc76013b201445beff3be320f5a2215
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-07-17 16:08:12 +0000

    2014 TFK digital change

--- docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
@@ -10,8 +10,25 @@
 */
 global $user;
 $cgiToken = ($user->uid ? $_COOKIE['CGI-token'] : '');
+#flog_it("cgiToken=".$cgiToken);
+#flog_it("_ENV['AH_SITE_ENVIRONMENT']:");
+#flog_it($_ENV['AH_SITE_ENVIRONMENT']);
+
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  switch ($_ENV['AH_SITE_ENVIRONMENT']) {
+    case 'prod':
+      $form_action = 'https://app.timeforkidsdigital.com/cgiAuth.php';
+      break;
+
+    default:
+      $form_action = 'https://qa-auth.timeforkids.com/cgiAuth.php';
+      break;
+  }
+} else {
+    $form_action = 'https://app.timeforkidsdigital.com/cgiAuth.php';
+}
 ?>
-<form id="cgi-redirect" action="https://app.timeforkidsdigital.com/cgiAuth.php" method="post">
+<form id="cgi-redirect" action="<?php echo $form_action; ?>" method="post">
 	<input type="hidden" name="CGI-Token-Exp" value="2592000000" />
 	<input type="hidden" name="CGI-Remember-Me" value="0" />
 	<input type="hidden" name="CGI-Token" value="<?php echo $cgiToken; ?>" />
