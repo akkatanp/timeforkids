14df9e599e1e72a06ab4219fa0d47b328d97a8ac
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-08-14 21:28:42 +0000

    2014 TFK Assessment Branch

--- docroot/sites/all/themes/tfk/templates/tfk-assessment.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-assessment.tpl.php
@@ -1,5 +1,17 @@
 <?php
     flog_it("tfk-assessment.tpl.php...");
+    // Test URL: 
+    // http://mobilesandbox.timeinc.com
+    // http://mobilesandbox.timeinc.com/tfk/cognero.html
+    if ($_SERVER['HTTP_HOST'] == "www.timeforkids.com") {
+        $cogneroDomain = "https://qa-tfkclassroomapp.timeinc.com";
+        $cogneroURL = "https://qa-tfkclassroomapp.timeinc.com/Instructor/SingleSignOn.aspx?authToken=";
+    } else {
+        $cogneroDomain = "https://qa-tfkclassroomapp.timeinc.com";
+        $cogneroURL = "https://qa-tfkclassroomapp.timeinc.com/Instructor/SingleSignOn.aspx?authToken=";
+    }
+    
+    $token = $_COOKIE['CGI-token'];
 ?>
 
 <script language="javascript">
@@ -8,26 +20,26 @@
     // http://tfk:8082/cognero.html
 
     function receiveSize(e) {
-        console.log("receiveSize: e.origin="+e.origin);
-        if (e.origin === "http://mobilesandbox.timeinc.com") {// for security: set this to the domain of the iframe - use e.uri if you need more specificity
-            console.log("found domain, attempting to resize...");
-            document.getElementById("tfk_assessment").style.height = e.data + 40 + "px";
+        //console.log("receiveSize: e.origin="+e.origin);
+        if (e.origin === "<?php echo $cogneroDomain; ?>") {// for security: set this to the domain of the iframe - use e.uri if you need more specificity
+            //console.log("found domain, attempting to resize...");
+            var obj = JSON.parse(e.data);
+            //console.log("obj.height="+obj.height+", obj.width="+obj.width)
+            document.getElementById("tfk_assessment").style.height = obj.height;
+            //document.getElementById("tfk_assessment").style.width = obj.width;
             //window.removeEventListener("message", receiveSize, false);
         }
     }
     
-    window.addEventListener("message", receiveSize, false);
-/*
     if (window.addEventListener) {
-        console.log("adding non-IE addEventListener...");
+        //console.log("adding non-IE addEventListener...");
         window.addEventListener("message", receiveSize, false);  
     } else {
-        console.log("adding IE addEventListener...");
+        //console.log("adding IE addEventListener...");
         window.attachEvent("onmessage", receiveSize);//for ie  
     } 
-*/
      
 </script>
 
-<iframe id="tfk_assessment" width="950" height="100" border="1" scrollbars="no" src="http://mobilesandbox.timeinc.com/tfk/cognero.html"></iframe>
+<iframe id="tfk_assessment" width="950" height="100" frameBorder="0" scrollbars="no" src="<?php echo $cogneroURL.$token; ?>"></iframe>
 <!--<iframe id="tfk_assessment" width="950" height="100" border="1" scrollbars="no" src="http://tfk:8082/cognero.html"></iframe>-->
