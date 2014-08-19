d326a2012efb1edcb8bbf11cf4b5723aeb163578
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-08-19 21:45:44 +0000

    2014 TFK Assessment

--- docroot/sites/all/themes/tfk/templates/tfk-goto-TCS.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-goto-TCS.tpl.php
@@ -5,6 +5,7 @@
 
 <html>
 <body>
+    <p style="font-family:arial;font-style:normal;font-size:14px;">LUCIE account without Assessment access. Bringing up the TCS sign-up page...<p>
     <a id="url" target="_blank" href="<?php echo $url; ?>"></a>
     <script type="text/javascript">
         window.onload=function() {
