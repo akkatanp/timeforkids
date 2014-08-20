8a7431edbafb0d9ac1120f53174d25c2e48d5445
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-08-20 16:39:52 +0000

    2014 TFK Assessment

--- docroot/sites/all/themes/tfk/templates/tfk-goto-TCS.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-goto-TCS.tpl.php
@@ -1,11 +1,11 @@
 <?php
 //flog_it("tfk-goto-TCS.tpl.php...");
-//flog_it("url=".$url);
+//flog_it("url=".$url);  
 ?>
 
 <html>
 <body>
-    <p style="font-family:arial;font-style:normal;font-size:14px;">LUCIE account without Assessment access. Bringing up the TCS sign-up page...<p>
+    <p style="font-family:arial;font-style:normal;font-size:14px;">TFK account without Assessment access. Bringing up the TCS sign-up page. Please disable popup blockers for the TCS sign-up page to appear.<p>
     <a id="url" target="_blank" href="<?php echo $url; ?>"></a>
     <script type="text/javascript">
         window.onload=function() {
