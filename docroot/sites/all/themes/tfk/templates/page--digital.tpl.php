dd2e32cce2d725f7f371a2f89b97ab9f0ae6aac9
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-08-21 16:41:25 +0000

    2014 TFK Reader

--- docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
@@ -19,7 +19,7 @@ flog_it($_ENV['AH_SITE_ENVIRONMENT']);
 if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
   switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     case 'prod':
-      $form_action = 'https://app.timeforkidsdigital.com/cgiAuth.php';
+      $form_action = 'https://auth.timeforkids.com/cgiAuth.php';
       break;
 
     default:
@@ -27,7 +27,7 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
       break;
   }
 } else {
-    $form_action = 'https://app.timeforkidsdigital.com/cgiAuth.php';
+    $form_action = 'https://auth.timeforkids.com/cgiAuth.php';
 }
 ?>
 <form id="cgi-redirect" action="<?php echo $form_action; ?>" method="post">
