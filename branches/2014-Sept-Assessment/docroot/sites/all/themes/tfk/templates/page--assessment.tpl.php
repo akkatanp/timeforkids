b22382ea8938ed298b4538916f11852c10ce2689
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-06 20:59:32 +0000

    2014 Sept Assessment Testing: Updated Assessment workflow

--- branches/2014-Sept-Assessment/docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
+++ branches/2014-Sept-Assessment/docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
@@ -90,7 +90,7 @@
     
     
     
-    //flog_it("tfk-assessment.tpl.php: email=".$user->mail);
+    flog_it("email=".$user->mail);
     flog_it("CGI-token=".$token);
     
     if ($_SERVER['HTTP_HOST'] == "www.timeforkids.com") {
@@ -103,35 +103,6 @@
     flog_it("cogneroDomain=".$cogneroDomain);
     flog_it("cogneroURL=".$cogneroURL);
 ?>
-
-<script language="javascript" type="text/javascript">
-    // http://jandcgroup.com/2011/10/17/dynamic-height-cross-domain-iframe-postmessage-javascript/
-    // http://mobilesandbox.timeinc.com/tfk/cognero.html
-    // http://tfk:8082/cognero.html
-
-    function receiveSize(e) {
-        //console.log("receiveSize: e.origin="+e.origin);
-        if (e.origin === "<?php echo $cogneroDomain; ?>") {// for security: set this to the domain of the iframe - use e.uri if you need more specificity
-            //console.log("found domain, attempting to resize...");
-            var obj = JSON.parse(e.data);
-            //console.log("obj.height="+obj.height+", obj.width="+obj.width)
-            document.getElementById("tfk_assessment").style.height = obj.height;
-            //document.getElementById("tfk_assessment").style.width = obj.width;
-            //window.removeEventListener("message", receiveSize, false);
-        }
-    }
-    
-    if (window.addEventListener) {
-        //console.log("adding non-IE addEventListener...");
-        window.addEventListener("message", receiveSize, false);  
-    } else {
-        //console.log("adding IE addEventListener...");
-        window.attachEvent("onmessage", receiveSize);//for ie  
-    } 
-</script>
-
-<!--<iframe name="tfk_assessment_name" id="tfk_assessment" width="950" height="100" frameBorder="0" scrollbars="no"></iframe>-->
-<!--<iframe id="tfk_assessment" width="950" height="100" border="1" scrollbars="no" src="http://tfk:8082/cognero.html"></iframe>-->
 
 <form id="cgi-redirect" action="<?php echo $cogneroURL; ?>" method="post">
 </form>
