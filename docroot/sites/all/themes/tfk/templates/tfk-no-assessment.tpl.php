480196186c627d94b94b87579036b58c234bcfba
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-16 16:19:39 +0000

    2014 TFK Oct Assessment

--- docroot/sites/all/themes/tfk/templates/tfk-no-assessment.tpl.php
+++ docroot/sites/all/themes/tfk/templates/tfk-no-assessment.tpl.php
@@ -1,7 +1,7 @@
 <?php
     global $user;
     
-    flog_it("tfk-no-assessment.tpl.php: email=".$user->mail);
-    flog_it("token=".$token);
+    //flog_it("tfk-no-assessment.tpl.php: email=".$user->mail);
+    //flog_it("token=".$token);
 ?>
 <p style="font-family:arial;font-style:normal;font-size:14px;">You do not have access to the TFK Assessment...</p>
