ebd90692d65b4234f395d3ff8beddcba5c31b5ed
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-16 16:12:09 +0000

    2014 TFK Oct Assessment

--- branches/2014-Sept-Assessment/docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
+++ branches/2014-Sept-Assessment/docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
@@ -1,6 +1,6 @@
 <?php
     global $user;
-    flog_it("page--assessment.tpl.php...");
+    //flog_it("page--assessment.tpl.php...");
     
     // Check for LUCIE token 
     $token = "";
@@ -8,7 +8,7 @@
         $token = $_COOKIE['CGI-token'];
     }
     
-    if ($_SERVER['HTTP_HOST'] != "www.timeforkids.com") {
+    if ($_SERVER['HTTP_HOST'] == "www.timeforkids.com") {
         $cogneroTokenURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/WebServices/Security.asmx/EncryptAuthToken";
         $cogneroURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/Instructor/SingleSignOn.aspx?authToken=";
         $wesURL = 'https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/app/login.jsp&account=';
@@ -18,14 +18,14 @@
         $wesURL = 'https://wesqa.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/app/login.jsp&account=';
     }
     
-    flog_it("email=".$user->mail);
-    flog_it("token=".$token);
-    flog_it("cogneroTokenURL=".$cogneroTokenURL);
-    flog_it("cogneroURL=".$cogneroURL);
-    flog_it("wesURL=".$wesURL);
+    //flog_it("email=".$user->mail);
+    //flog_it("token=".$token);
+    //flog_it("cogneroTokenURL=".$cogneroTokenURL);
+    //flog_it("cogneroURL=".$cogneroURL);
+    //flog_it("wesURL=".$wesURL);
 
     if ($_COOKIE['Assessment-access'] === "no" && $token != "") {
-        flog_it("No Assessment access. Check with LUCIE again...");
+        //flog_it("No Assessment access. Check with LUCIE again...");
         
         $domain = variable_get('lucie_callback_domain', 'qa-lucie.timeinc.com');
         $url = 'https://'. $domain. '/webservices/entitlements?CGI-App-Id=com.timeinc.tk.web';
@@ -41,19 +41,19 @@
         // Check for TFK Assessment access
         // Get the LUCIE account number
         $tcsAccountNumber = $dataResponse['issues'][0]['tcsAccountNumber'];
-        flog_it("url=".$url);
-        flog_it("tcsAccountNumber=".$tcsAccountNumber);
+        //flog_it("url=".$url);
+        //flog_it("tcsAccountNumber=".$tcsAccountNumber);
         
         if ($tcsAccountNumber === "COMP") {
-            flog_it("Has COMP access...");
+            //flog_it("Has COMP access...");
             tfk_assessment_access_set_cookie("yes");
         } elseif ($tcsAccountNumber === null) {
-            flog_it("Not a LUCIE member!!!");
+            //flog_it("Not a LUCIE member!!!");
             drupal_goto("no-assessment");
         } else {
             $appIds = $dataResponse['tiersInfo']['availableAppIds'];
-            flog_it("apIds:");
-            flog_it($apIds);
+            //flog_it("apIds:");
+            //flog_it($apIds);
             $appIdArr = split(',', $appIds);
             $found = 0;
             foreach ($appIdArr as $key=>$value) {
@@ -66,10 +66,10 @@
 
             // Process results
             if ($found && is_numeric($tcsAccountNumber)) {
-                flog_it("User has Assessment access.");
+                //flog_it("User has Assessment access.");
                 tfk_assessment_access_set_cookie("yes");
             } else {
-                flog_it("No Assessment Access. Goto TCS page...");
+                //flog_it("No Assessment Access. Goto TCS page...");
                 tfk_assessment_access_set_cookie("no");
                 // Goto the TCS sign-up page
                 $url = $wesURL.$tcsAccountNumber;
@@ -77,12 +77,12 @@
             }
         }
     } elseif ($_COOKIE['Assessment-access'] === "no") {
-        flog_it("Final: No Assessment access");
+        //flog_it("Final: No Assessment access");
         drupal_goto("no-assessment");   
     }
     
     // Cognero processing
-    flog_it("Has Assessment Access, Cognero processing...");
+    //flog_it("Has Assessment Access, Cognero processing...");
     
     $data = array(
       'authToken' => $token  
@@ -92,7 +92,7 @@
         'data'=>drupal_json_encode($data), 'method'=>'POST'
     ));
     
-    flog_it("Response code=".$response->code);
+    //flog_it("Response code=".$response->code);
     
     // Check for Cognero down
     if ($response->code != 200) {
