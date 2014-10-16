d440f552ae619c25b7fe02890ce3d65517f43d24
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-16 23:51:47 +0000

    2014 Oct Assessment Branch 3

--- docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page--digital.tpl.php
@@ -8,13 +8,12 @@
 /*
 *	Set the post fields to sent to the digital product page
 */
-global $user;  
+global $user; 
+//flog_it($user);
 $cgiToken = ($user->uid ? $_COOKIE['CGI-token'] : '');
-flog_it("cgiToken=".$cgiToken);
-flog_it($_ENV['AH_SITE_ENVIRONMENT']);
-
-//https://app.timeforkidsdigital.com/cgiAuth.php
-//http://subscription-assets.timeforkids.com/prod/assets/themes/magazines/SUBS/templates/velocity/site/tk-digital/library.html
+watchdog('page--digital.tp.php', 'user name=%name, user email=%email, CGI-token=%cgiToken', array('%name' => $user->name, '%email' => $user->mail, '%cgiToken' => $cgiToken));
+//flog_it("cgiToken=".$cgiToken);
+//flog_it($_ENV['AH_SITE_ENVIRONMENT']);
 
 if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
   switch ($_ENV['AH_SITE_ENVIRONMENT']) {
@@ -27,8 +26,10 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
       break;
   }
 } else {
-    $form_action = 'https://auth.timeforkids.com/cgiAuth.php';
+    $form_action = 'https://qa-auth.timeforkids.com/cgiAuth.php';
 }
+
+//watchdog('page--digital.tp.php', 'form_action=%form_action', array('%form_action' => $form_action));
 ?>
 <form id="cgi-redirect" action="<?php echo $form_action; ?>" method="post">
 	<input type="hidden" name="CGI-Token-Exp" value="2592000000" />
