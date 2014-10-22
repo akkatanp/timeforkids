1826a6c54e4362c2e5dc7a73fa210640d98f0615
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-22 05:30:08 +0000

    2014 TFK Drupal 7.32

--- branches/2014-Sept-Assessment/docroot/sites/all/modules/contrib/video/theme/video-play-windowsmedia.tpl.php
+++ branches/2014-Sept-Assessment/docroot/sites/all/modules/contrib/video/theme/video-play-windowsmedia.tpl.php
@@ -1,23 +1,20 @@
 <?php
-/**
+/*
  * @file
  * Theme file to handle windows media output.
- *
- * Variables passed:
- * $item
- * $width
- * $height
- * $autoplay
- * $autobuffering
+ * 
+ * Variables passed.
+ * $video is the video object.
+ * $node is the node object.
+ * 
  */
-
-$url = check_plain(file_create_url($item['playablefiles'][0]->uri));
 ?>
-<object type="video/x-ms-wmv" data="<?php print $url; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>">
-  <param name="src" value="<?php print $url; ?>" valuetype="ref" type="<?php print $url; ?>">
+
+<object type="video/x-ms-wmv" data="<?php print $video->files->{$video->player}->url; ?>" width="<?php print $video->player_width; ?>" height="<?php print $video->player_height; ?>">
+  <param name="src" value="<?php print $video->files->{$video->player}->url; ?>" valuetype="ref" type="<?php print $video->files->{$video->player}->url; ?>">
   <param name="animationatStart" value="true">
   <param name="transparentatStart" value="true">
-  <param name="autostart" value="<?php print $autoplay ? 'true' : 'false'; ?>">
+  <param name="autostart" value="<?php print $video->autoplay; ?>">
   <param name="controller" value="1">
-  <?php print t('No video? Get the !plugin', array('!plugin' => l(t('Windows Media plugin'), url('http://www.microsoft.com/windows/windowsmedia/player/download/')))); ?>
+  <?php print t('No video?  Get the Windows Media !plugin', array('!plugin' => l(t('Plugin'), 'http://www.microsoft.com/windows/windowsmedia/player/download/'))); ?>
 </object>
