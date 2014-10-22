9955b62c9d08f9859c504b7367eb5b2b905bf848
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2014-10-22 23:41:31 +0000

    2014 Oct Drupal7.32.1

---
+++ tags/2014-Oct-Drupal7.32.1/docroot/sites/all/themes/tfk/js/atw-native-lingo.js
@@ -0,0 +1,44 @@
+(function($) {
+	$(document).ready(function() {
+		$("#audio-player").jPlayer({
+			swfPath: '/sites/all/themes/tfk/js/jplayer',
+			ready: function() {
+				$('#native-lingo-container').nativeLingo();
+			}
+		});
+	});
+	
+	$.fn.nativeLingo = function() {
+		var basics = $(this).find('#basics .phrase a.audio-link');
+		var nextLevel = $(this).find('#next-level .phrase a.audio-link');
+		
+		var activateClip = function(e) {
+			e.preventDefault();
+			$('#phrase-text').text($(this).text());
+			$('#phrase-translation').text($(this).attr('rel'));
+			$("#audio-player").jPlayer('setFile', $(this).attr('href')).jPlayer('play');
+		};
+		
+		if (basics.length) {
+			basics.each(function(i, clip) {
+				if (i == 0) {
+					$('#phrase-text').text($(clip).text());
+					$('#phrase-translation').text($(clip).attr('rel'));
+					$("#audio-player").jPlayer('setFile', $(clip).attr('href'));
+				}
+				$(clip).click(activateClip);
+			});
+		}
+		
+		if (nextLevel.length) {
+			nextLevel.each(function(i, clip) {
+				if (i == 0 && !basics.length) {
+					$('#phrase-text').text($(clip).text());
+					$('#phrase-translation').text($(clip).attr('rel'));
+					$("#audio-player").jPlayer('setFile', $(clip).attr('href'));
+				}
+				$(clip).click(activateClip);
+			});
+		}
+	};
+})(jQuery);
