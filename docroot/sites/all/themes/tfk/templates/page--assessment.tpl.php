45589bf5579b841e59f57b38fbfbbe148189258c
tpaige <tpaige@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-01-26 18:07:20 +0000

    2015 Jan TFK Updated no assessment code

--- docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
+++ docroot/sites/all/themes/tfk/templates/page--assessment.tpl.php
@@ -1,93 +1,35 @@
 <?php
     global $user;
     //flog_it("page--assessment.tpl.php...");
-    
+ 
     // Check for LUCIE token 
     $token = "";
-    if (array_key_exists("CGI-token", $_COOKIE)) {
+    if ( isset($_COOKIE) && array_key_exists("CGI-token", $_COOKIE)) {
         $token = $_COOKIE['CGI-token'];
     }
     
-    if ($_SERVER['HTTP_HOST'] == "www.timeforkids.com") {
-        $cogneroTokenURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/WebServices/Security.asmx/EncryptAuthToken";
-        $cogneroURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/Instructor/SingleSignOn.aspx?authToken=";
-        $wesURL = 'https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/app/login.jsp&account=';
-    } else {
-        $cogneroTokenURL = "https://qa-tfkclassroomapp.timeinc.com/TimeForKids/WebServices/Security.asmx/EncryptAuthToken";
-        $cogneroURL = "https://qa-tfkclassroomapp.timeinc.com/TimeForKids/Instructor/SingleSignOn.aspx?authToken=";
-        $wesURL = 'https://wesqa.customersvc.com/servlet/Show?WESPAGE=am/tablet/tk/app/login.jsp&account=';
-    }
-    
-    //flog_it("email=".$user->mail);
-    //flog_it("token=".$token);
-    //flog_it("cogneroTokenURL=".$cogneroTokenURL);
-    //flog_it("cogneroURL=".$cogneroURL);
-    //flog_it("wesURL=".$wesURL);
-    //flog_it("Cookie: Assessment-access=".$_COOKIE['Assessment-access']);
-    //flog_it(isset($_COOKIE));
-
-    if ((isset($_COOKIE) && $_COOKIE['Assessment-access'] === "no") && $token != "") {
-        //flog_it("No Assessment access. Check with LUCIE again...");
-        
-        $domain = variable_get('lucie_callback_domain', 'qa-lucie.timeinc.com');
-        $url = 'https://'. $domain. '/webservices/entitlements?CGI-App-Id=com.timeinc.tk.web';
-        $response = drupal_http_request($url, array('headers' => array('Content-Type'=>'application/json', 'CGI-token' => $token),
-              'method'=> 'GET'
-        ));
-        //flog_it($response);
-
-        // Decode the JSON data and pass to the call to get the user information
-        $dataResponse = drupal_json_decode($response->data);
-        //flog_it($dataResponse);
-
-        // Check for TFK Assessment access
-        // Get the LUCIE account number
-        if (array_key_exists(0, $dataResponse['issues'])) {
-            $tcsAccountNumber = $dataResponse['issues'][0]['tcsAccountNumber'];
-        } else {
-            $tcsAccountNumber = null;
-        }
-        //flog_it("url=".$url);
-        //flog_it("tcsAccountNumber=".$tcsAccountNumber);
-        
-        if ($tcsAccountNumber === "COMP") {
-            //flog_it("Has COMP access...");
-            tfk_assessment_access_set_cookie("yes");
-        } elseif ($tcsAccountNumber === null) {
-            //flog_it("Not a LUCIE member!!!");
-            drupal_goto("no-assessment");
-        } else {
-            $appIds = $dataResponse['tiersInfo']['availableAppIds'];
-            //flog_it("apIds:");
-            //flog_it($apIds);
-            $appIdArr = split(',', $appIds);
-            $found = 0;
-            foreach ($appIdArr as $key=>$value) {
-                if ($value == "com.timeinc.tfk.apple") {
-                    //flog_it("Found com.timeinc.tfk.apple...");
-                    $found = 1;
-                    break;
-                }
-            }
-
-            // Process results
-            if ($found && is_numeric($tcsAccountNumber)) {
-                //flog_it("User has Assessment access.");
-                tfk_assessment_access_set_cookie("yes");
-            } else {
-                //flog_it("No Assessment Access. Goto TCS page...");
-                tfk_assessment_access_set_cookie("no");
-                // Goto the TCS sign-up page
-                $url = $wesURL.$tcsAccountNumber;
-                drupal_goto($url);
-            }
-        }
-    } elseif (isset($_COOKIE) && $_COOKIE['Assessment-access'] === "no") {
+    // Check for Assessment access
+    if ((isset($_COOKIE) && $_COOKIE['Assessment-access'] == "no") || $token == "") {
         //flog_it("Final: No Assessment access");
         drupal_goto("no-assessment");   
     }
     
-    // Cognero processing
+    
+    // Continue processing...
+    if ($_SERVER['HTTP_HOST'] == "www.timeforkids.com") {
+        $cogneroTokenURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/WebServices/Security.asmx/EncryptAuthToken";
+        $cogneroURL = "https://tfkclassroomapp.timeinc.com/TimeForKids/Instructor/SingleSignOn.aspx?authToken=";
+    } else {
+        $cogneroTokenURL = "https://qa-tfkclassroomapp.timeinc.com/TimeForKids/WebServices/Security.asmx/EncryptAuthToken";
+        $cogneroURL = "https://qa-tfkclassroomapp.timeinc.com/TimeForKids/Instructor/SingleSignOn.aspx?authToken=";
+    }
+
+    //flog_it("email=".$user->mail);
+    //flog_it("token=".$token);
+    //flog_it("cogneroTokenURL=".$cogneroTokenURL);
+    //flog_it("cogneroURL=".$cogneroURL);
+    //flog_it("Cookie: Assessment-access=".$_COOKIE['Assessment-access']);
+    //flog_it(isset($_COOKIE));
     //flog_it("Has Assessment Access, Cognero processing...");
     
     $data = array(
