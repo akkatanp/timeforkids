fe2308fa775403e3a4794a20aa4c351abfeaa347
pranita <pranita@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-06-05 13:09:14 +0000

    Remove Login popup for Anonymous users to access "/digital" path.

---
+++ tags/2015-June-Release.2/docroot/sites/all/modules/custom/tfk_print/tfk_print.tpl.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *   ~ tfk_print.tpl.php ~
+ *   The view tpl for the tfk_print module.
+ *   Variables are loaded from tfk_print.module.
+ */
+// Clean title if necessary
+$head_title = strip_tags(check_markup($head_title));
+?>
+<!doctype html>
+<html>
+    <meta charset="utf-8">
+    <title><?=$title?> | TIME For Kids</title>
+    <link rel="stylesheet" type="text/css" href="/<?=drupal_get_path('module', 'tfk_print') .'/css/style.css';?>">
+<head>
+</head>
+<body>
+    <div id = "wrapper">
+    
+        <!-- Logo -->
+        <a id = "logo" href = "/">
+            <img src = "/<?=drupal_get_path('module', 'tfk_print') .'/img/tfk-logo.png';?>" alt="TIME For Kids">
+        </a>
+    
+        <!-- Headline & Deck -->
+        <h1><?=$title?></h1>
+        <h2><?=$deck?></h2>
+        <div id = "byline"><span class = "date"><?=$date?></span> | <span class = "author"><?=$byline?></span></div>
+        
+        <!-- Body -->
+        <div id = "body" class = "cf">       
+            <?=$photo?><?=$body?>
+        </div>
+
+        <!-- Footer -->
+        <div id = "footer">
+            <?=$backlink?>
+        </div>
+    
+    </div>
+</body>
+</html>
