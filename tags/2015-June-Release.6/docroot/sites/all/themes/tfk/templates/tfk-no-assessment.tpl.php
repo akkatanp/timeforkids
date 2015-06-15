86e73022c9ac3c87078af550dcae77b5f9b88f4c
pranita <pranita@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-06-15 13:28:36 +0000

    #321 - QA changes for border, image link and News Archive for Anonymous users.

---
+++ tags/2015-June-Release.6/docroot/sites/all/themes/tfk/templates/tfk-no-assessment.tpl.php
@@ -0,0 +1,4 @@
+<?php
+    global $user;
+?>
+<p style="font-family:arial;font-style:normal;font-size:14px;">You do not have access to the TFK Assessment.<br/>To update or change your subscription options please call 1-800-777-8600</p>
