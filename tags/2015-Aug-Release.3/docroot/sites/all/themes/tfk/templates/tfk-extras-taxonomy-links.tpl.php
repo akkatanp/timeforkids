59fedd2a43c913398a15d96b50bb16a57e9fb955
pranita <pranita@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-08-28 10:25:57 +0000

    tag before commit

--- tags/2015-Aug-Release.3/docroot/sites/all/themes/tfk/templates/tfk-extras-taxonomy-links.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php echo l('PROGRAMS','extras');?>
-
-<ul class="menu">
-<?php foreach($terms_with_articles as $term): ?>
-  <li><?php echo l($term['term_name'],'extras/'.strtolower($term['term_name']),array('attributes' => array('class'=>array($term['class']))));?></li>
-<?php endforeach; ?>
-</ul>
