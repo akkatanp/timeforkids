7813f823abd836c3a0ec1c6bf4533d28c8f28678
pranita <pranita@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-09-25 09:59:05 +0000

    Delete and create branch from latest master

--- branches/2015-pranita-tfk-232/docroot/sites/all/themes/tfk/templates/tfk-no-assessment.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php
-    global $user;
-?>
-<p style="font-family:arial;font-style:normal;font-size:14px;">You do not have access to the TFK Assessment.<br/>To update or change your subscription options please call 1.800.777.8600</p>
--- branches/2015-pranita-tfk-232/docroot/sites/all/themes/tfk/templates/tfk-search-back-btn.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php if($show == 1):?>
-<div class="<?php echo $class;?>">
-  <a href="<?php echo $dest;?>"><?php echo $copy;?></a>
-</div>
-<?php endif;?>
