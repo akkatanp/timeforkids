366fc6793275b0dd6e920a9b12066e8dff885c4d
pranita <pranita@ef34a74e-757f-47cd-94cb-817147fcdc54>
2015-12-07 10:03:40 +0000

    TFK-393-Title

--- docroot/sites/all/themes/tfk/templates/html.tpl.php
+++ docroot/sites/all/themes/tfk/templates/html.tpl.php
@@ -61,7 +61,7 @@
  */
 
 // Clean title if necessary
-$head_title = strip_tags(check_markup($head_title));
+$head_title = html_entity_decode(strip_tags(check_markup($head_title)));
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> class="no-js">
